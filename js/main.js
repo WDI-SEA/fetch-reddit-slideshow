f2799b9233118e0532442d3563330565ab23cb73
009kings <009kings@gmail.com>
2018-12-05 23:56:20 -0800

    Functionality present, aesthetics taking a nap

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // Function that adds the images
 function addImage(jsonImg) {
-    let li = document.createElement("li");
-    let img = document.createElement("img");
-    img.src = jsonImg;
-    document.getElementById("search-results").appendChild(li).appendChild(img);
+    $("#search-results").slick(`slickAdd`, `<div class="img-container"><img class="img-result" src=${jsonImg}></div>`);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -14,12 +11,6 @@ document.addEventListener("DOMContentLoaded", function(){
         
         // Set our API
         let requestURL = `http://www.reddit.com/search.json?q=${form}&limit=30&post-hint=image`;
-
-        // Lets clear the form
-        var searchResults = document.querySelectorAll("li");
-        for (let i = 0; i < searchResults.length; i++) {
-            searchResults[i].remove();
-        }
 
         // Let's make fetch happen
         fetch(requestURL)
@@ -36,18 +27,27 @@ document.addEventListener("DOMContentLoaded", function(){
                         }
                     })
                 newArray.forEach(addImage);
-                console.log(newArray);
             }).catch(function(error){
                 console.log("Yikes, you got an error matey", error);
             })
         // Hide and Seek where the seek is a button called reset-btn
         $("#start-state").addClass("hidden");
+
+        // Slideshow goes here
+        $('#search-results').slick({
+              autoplay: true
+            });
     })
     $("#reset-btn").click(function(){
         $("#start-state").removeClass("hidden");
+        // Lets clear the form
+        var searchResults = document.querySelectorAll(".img-container");
+        for (let i = 0; i < searchResults.length; i++) {
+            searchResults[i].remove();
+        }
         console.log("Reseting things");
     })
     $("#stop-slideshow").click(function(){
-        console.log("slideshow stopped");
+        $('#search-results').slick("slickPause");
     })
 })
