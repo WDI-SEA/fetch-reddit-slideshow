fa275d1f98751144a4154261e7fb1b7464010f84
009kings <009kings@gmail.com>
2018-12-05 21:55:31 -0800

    Gets pictures, demands the whole page which is a feature

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
+// Function that adds the images
+function addImage(jsonImg) {
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    img.src = jsonImg.data.url;
+    document.getElementById("search-results").appendChild(li).appendChild(img);
+}
+
 document.addEventListener("DOMContentLoaded", function(){
     $("#search-form").submit(function(e){
         e.preventDefault();
-        // I will probably have to figure out how to change this to a string with no whitespace, but plusses instead.
-        let form = $("#search-input").val();
+        // form input becomes form
+        let form = encodeURIComponent($("#search-input").val());
         
         // Set our API
-        let requestURL = `http://www.reddit.com/search.json?q=${form}&limit=20`;
+        let requestURL = `http://www.reddit.com/search.json?q=${form}&limit=20&post-hint=image`;
+
+        // Lets clear the form
+        var searchResults = document.querySelectorAll("li");
+        for (let i = 0; i < searchResults.length; i++) {
+            searchResults[i].remove();
+        }
 
         // Let's make fetch happen
         fetch(requestURL)
             .then(function (responseData) {
                 return responseData.json();
             }).then(function (jsonData) {
-                // What we do in the Json
-                console.log(jsonData);
+                // What we do in the Json: Print deez puppies
+                let jsonDataArray = jsonData.data.children;
+                jsonDataArray.forEach(addImage);
+                console.log(jsonDataArray);
             }).catch(function(error){
                 console.log("Yikes, you got an error matey", error);
             })
