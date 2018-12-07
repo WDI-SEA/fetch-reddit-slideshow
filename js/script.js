67abcef04d6b0ee46737f627cb42cea4d1f8a785
Nhuuu <nqtrinh87@gmail.com>
2018-12-07 09:38:00 -0800

    reddit completed

--- js/script.js
+++ js/script.js
@@ -1,41 +1,120 @@
-document.addEventListener("DOMContentLoaded", function(){
-	console.log("yay");
-	//instead of immediately loading the tags, store results array into a variable declared 
+let results;
+let currentIndex;
+let slideshow;
 
-	//use this main results to store but can also use to clear.
-	//need to use set interval to change src tag or carousel to spin on a set interval 
 
-	const resultsArr = [];
-	const ul = document.getElementById("search-results");
-
-	function addImg(picture){
-		let li = document.createElement("li");
-		let img = document.createElement("img");
-		img.src = picture.url;
-		ul.appendChild(li).appendChild(img);
+	function retrieveImgUrls(item){
+		//return what we want to be in the new array.
+		return item.data.url
 	}
 
-	document.getElementById("search-form").addEventListener("submit", function(e){
-		e.preventDefault();
-		console.log(document.querySelector('input').value);
-		let userInput = document.querySelector('input').value;
-		let requestURL = `http://www.reddit.com/search.json?q=${userInput}+nsfw:no`;
-		
+	function isAnImg(url){
+		//creating a new array that only containts urls with i.imgur bc we know these are the imgs that work
+		return url.includes("i.imgur")|| url.includes("i.redd");
+	}
 
-	fetch(requestURL)
-		.then(function(responseData){
-			return responseData.json();
-		})
-		.then(function(jsonData){
-			console.log(jsonData.data.children[0].data[1].thumbnail);
-			// console.log(jsonData[0].data.children.data.preview.images[0].url); 
-			// var imgOnly = jsonData.filter(function(pic){
-			// 	return pic[0].data.children.data.preview.images[0].url == ".jpg" || ".png" || ".gif" {
-			// });
-			// jsonData.forEach(addImg);
-		})
-		.catch(function(err){
-			console.log("Oh no!", err);
-		});
+	function convertGif(onceFilteredUrls){
+		return onceFilteredUrls.replace(".gifv", ".gif");
+	}
+
+	function slide(){
+		if(currentIndex === results.length -1){
+			//to reset to 0
+			currentIndex = -1;
+		}
+		currentIndex++;
+		//change the src attribute of the img
+		let currentImg = document.querySelector("img");
+		currentImg.src = results[currentIndex];
+	}
+
+	function startCarousel(){
+		slideshow = setInterval(function(){
+		M.Carousel.getInstance(document.querySelector(".carousel")).next();
+		}, 3000)
+	}
+
+	function clearsCarousel(){
+		let carouselParent = document.querySelector(".carousel");
+		while(carouselParent.firstChild){
+			carouselParent.removeChild(carouselParent.firstChild);
+		}
+		clearInterval(slideshow);
+	}
+
+
+	function makeFetchHappen(url){
+
+		let imgUrls, onlyImgs;
+
+		fetch(url)
+			.then(function(responseObject){ 
+				return responseObject.json();
+			})
+			.then(function(jsonResults){
+				//store the array of results objects
+				let resultsObjectsArr = jsonResults.data.children;
+				//map the result objects array to an array of just urls from inside the objects
+				imgUrls = resultsObjectsArr.map(retrieveImgUrls)
+				onlyImgs = imgUrls.filter(isAnImg);
+				//replaces my results 
+				results = onlyImgs.map(convertGif);
+				// return results;
+
+				clearsCarousel();
+
+				results.forEach(function(resultUrl){
+					//create an anchor tag
+					let newAnchor = document.createElement("a");
+					//create an image tag
+					let newImg = document.createElement("img");
+					//add # href to anchor tag
+					newAnchor.href = "#";
+					//add class to carousel-item to anchor
+					newAnchor.classList.add("carousel-item");
+					//add src to the img tag
+					newImg.src = resultUrl;
+					//append img tag to anchor tag
+					newAnchor.appendChild(newImg);
+					//append anchor tag to carousel div
+					document.querySelector(".carousel").appendChild(newAnchor);
+				});
+			    var elems = document.querySelectorAll('.carousel');
+			    var instances = M.Carousel.init(elems);
+			    startCarousel();
+
+
+			})
+			// .then(function(){
+				// get rid of old slideshow
+				// let oldSlides = document.querySelector("img");
+				// if(oldSlides){
+				// 	clearInterval(slideshow);
+				// 	document.body.removeChild(oldSlides)
+				// }
+
+				// currentIndex = 0;
+				// let firstPhoto = document.createElement("img");
+				// firstPhoto.src = results[currentIndex];
+				// document.body.appendChild(firstPhoto);
+				// //slideshow needs to start after the first photo, change the sort attribute. (function, millisecs)
+				// slideshow = setInterval(slide, 3000);
+
+			// })
+			.catch(function(err){
+				console.log("Oh no!", err);
+			});
+	}	
+
+document.addEventListener("DOMContentLoaded", function(){
+	console.log("yay");
+	//listen for the submit event and prevent it from reloading on submission.
+	document.querySelector("#search-form").addEventListener("submit", function(e){
+		e.preventDefault();
+		//grab user input string
+		let userInput = document.querySelector('input').value; //could also use the #id, this shows the first input tag.
+		//storing api request url with user input into request url variable
+		let requestURL = `http://www.reddit.com/search.json?q=${userInput}&limit=100+nsfw:no`;
+		makeFetchHappen(requestURL);
 	});
 });
