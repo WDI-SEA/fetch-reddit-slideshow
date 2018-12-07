de1c0914e0da15284068f43968513ff1fd749cae
Parker Couch <parkercouch@gmail.com>
2018-12-06 16:17:28 -0800

    Non-working animation refactor

--- js/app.js
+++ js/app.js
@@ -1,95 +1,136 @@
-/* eslint-disable */
-$( document ).ready(function() {
-  $('.search-form').submit(go);
-});
+/* eslint-env jquery */
+/* eslint no-use-before-define: 1 */
 
-// addImages :: [String] -> Void
-const addImages = function (srcList) {
-  srcList.forEach((src) => {
-    $('#slideshow').append(`<div><img src="${src}"></div>`);
-  });
-};
-
-// filter through reddit data to grab some usable images
-const redditFilter = function (json) {
-  return json.data.children
-         .map((item) => item.data.url)
-         .filter((url) => url.includes('i.imgur'))
-         .map((url) => url.replace('.gifv', '.gif'))
-};
+// --------------------------------------------------- //
+// -------------------HEAVY HITTERS------------------- //
+// --------------------------------------------------- //
+/* #region */
 
 // fetchPhotos :: String -> Void
 const fetchPhotos = function (url) {
   fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (myJson) {
-      const urls = redditFilter(myJson); 
-
-      if (urls.length < 10) {
-        throw 'Not enough images';
+    .then(response => response.json())
+    .then((myJson) => {
+      const urls = redditImageFilter(myJson);
+      if (urls.length < 1) {
+        throw Error('Not enough images');
+      } else {
+        return urls;
       }
-
-      addImages(urls);
+    })
+    .then((imageUrls) => {
+      // add images to hidden slideshow div and start slideshow
+      addImages(imageUrls);
       startSlideshow();
-    })
-    .catch(function (error) {
-      console.log(error);
-      $('#title div p').text("Not enough images found. Try again!");
-      retry();
-    })
 
+
+      // Can this go somewhere else?
+      return 0;
+    })
+    .then((x) => {
+      // fade out loading icon div and fade in slideshow div
+      $('#loader').removeClass('hide');
+      toggleHide('#slideshow');
+
+      $('#title div p').text('I want to see photos of:');
+    })
+    .catch((error) => {
+      console.log(error);
+      $('#title div p').text('Not enough images found. Try again!');
+      retry();
+    });
 };
 
+/* #endregion */
+
+
+// --------------------------------------------------- //
+// -------------------CLICK EVENTS-------------------- //
+// --------------------------------------------------- //
+/* #region */
+
 // Click event for Go button
+// go :: Event -> Void
 const go = function (evt) {
   evt.preventDefault();
+
+  // get search item and cancel if blank
   const searchItem = $('#search').val();
   if (searchItem === '') return;
-  fetchPhotos(`https://www.reddit.com/search.json?q=${searchItem}+nsfw:no&limit=100&type=link`)
 
+  // Start image fetch
+  // UPDATE TO ADD LOADING ICON HERE?
+  fetchPhotos(`https://www.reddit.com/search.json?q=${searchItem}+nsfw:no&limit=100&type=link&sort=new`);
+
+  // maybe add div with loading icon?
+  // $('#slideshow-container').append('<div id="loading"><img src="slick/ajax-loader.gif"></div>');
+
+  toggleHide('#title', '#input-row', '#loading');
+  $('#loading').switchClass('small', 'big');
+  switchButton('go', 'stop');
+
+  // Reset search box to blank
   $('#search').attr('value', '');
-
-  $('#title').fadeOut(1000, function() {
-    $(this).addClass('hide');
-  });
-  $('#input-row').fadeOut(1000, function() {
-    $(this).addClass('hide');
-  });
-
-  $('.go').switchClass('go', 'stop');
-  $('.stop').attr('value', 'STOP');
-  $('.search-form').off('submit');
-  $('.search-form').submit(stop);
 };
 
 // Click event for Stop button
+// stop :: Event -> Void
 const stop = function (evt) {
   evt.preventDefault();
+  // Stop slideshow then reset
   $('#slideshow').slick('unslick');
   retry();
 };
 
-// Go back to start (either from click or failed fetch)
-const retry = function () {
-  $('#slideshow-container').empty();
-  $('#slideshow-container').append(`<div id="slideshow"></div>`);
+// const loading = function () {
+//   // $('#slideshow').fadeIn(1000, function fadeAppend() {
+//   //   $(this).append('<div class="hide" id="loading"><img src="slick/ajax-loader.gif"></div>');
+//   // });
+//   $('#slideshow').append('<div class="hide" id="loading"><img src="slick/ajax-loader.gif"></div>');
+// };
 
-  $('#title').fadeIn(1000, function() {
-    $(this).removeClass('hide');
-  });
-  $('#input-row').fadeIn(1000, function() {
-    $(this).removeClass('hide');
-  });
+/* #endregion */
 
-  $('.stop').switchClass('stop', 'go');
-  $('.go').attr('value', 'GO');
+
+// --------------------------------------------------- //
+// ----------------DOM MANIPULATION------------------- //
+// --------------------------------------------------- //
+/* #region */
+
+// Hide/Unhide
+// toggleHide :: [...String:selector] -> Void
+const toggleHide = function (...elements) {
+  elements.forEach((elem) => {
+    $(elem).fadeToggle(1000, function hide() {
+      $(this).toggleClass('hide');
+    });
+  });
+};
+
+// Switch Go-Stop button
+// switchButton :: String -> String -> Void
+const switchButton = function (from, to) {
+  $(`.${from}`).switchClass(`${from}`, `${to}`);
+  $(`.${to}`).attr('value', `${to.toUpperCase()}`);
   $('.search-form').off('submit');
-  $('.search-form').submit(go);
+
+  if (to === 'go') {
+    $('.search-form').submit(go);
+  } else {
+    $('.search-form').submit(stop);
+  }
+};
+
+// Add images to DOM
+// addImages :: [String] -> Void
+const addImages = function (srcList) {
+  srcList.forEach((src) => {
+    $('#slideshow').append(`<div><img class="slides" src="${src}"></div>`);
+  });
 };
 
 // Enable slick slideshow
+// startSlideshow :: () -> Void
 const startSlideshow = function () {
   $('#slideshow').slick({
     slidesToScroll: 1,
@@ -98,7 +139,60 @@ const startSlideshow = function () {
     pauseOnHover: true,
     fade: true,
     cssEase: 'linear',
-    dots: true,
+    dots: false,
   });
 };
 
+// Go back to start (either from click or failed fetch)
+// retry :: () -> Void
+const retry = function () {
+  // destroy slideshow and start over with empty one
+  toggleHide('#slideshow')
+
+  // $('#slideshow-container').empty();
+  emptySlideshow('#slideshow-container');
+  // $('#slideshow-container').append('<div class="hide" id="slideshow"></div>');
+
+  toggleHide('#title', '#input-row');
+  switchButton('stop', 'go');
+};
+
+// Fade out then delete
+// fadeToEmpty :: String:selector -> Void
+const emptySlideshow = function (elem) {
+  $(elem).fadeToggle(1000, function hide() {
+    $(this).empty();
+    $(this).append('<div class="hide" id="slideshow"></div>');
+  });
+};
+
+/* #endregion */
+
+
+// --------------------------------------------------- //
+// ----------------HELPER FUNCTIONS------------------- //
+// --------------------------------------------------- //
+/* #region */
+
+// filter through reddit data to grab some usable images
+// redditImageFilter :: json:Object -> [String]
+const redditImageFilter = function (json) {
+  return json.data.children
+         .map(item => item.data.url)
+         .filter(url => url.includes('i.imgur') || url.includes('i.red'))
+         .map(url => url.replace('.gifv', '.gif'));
+};
+
+/* #endregion */
+
+
+// --------------------------------------------------- //
+// -------------------DOM LOADED---------------------- //
+// --------------------------------------------------- //
+/* #region */
+
+$(document).ready(() => {
+  $('.search-form').submit(go);
+});
+
+/* #endregion */
