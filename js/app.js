3c99a7eca955c2b9676b6a6f8523929244a370a9
stanstaton <thatkidenzo@gmail.com>
2019-07-30 17:01:45 -0700

    pics are showing up

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 console.log("I'm loaded");
+
+document.getElementById("photo-form").addEventListener("submit", function(event){
+	event.preventDefault();
+	console.log("Form submitted");
+	var searchQuery = document.getElementById("query").value;
+
+	if (searchQuery) {
+		console.log(searchQuery);
+		document.getElementById("no-query").textContent = " ";
+
+		//search for the photos
+		fetch("https://www.reddit.com/search.json?q="+searchQuery)
+		.then(function(responseData) {
+			console.log(responseData);
+			return responseData.json();
+		})
+		.then(function (jsondata) {
+			console.log(jsondata);
+			jsondata.data.children.forEach(function(listing) {
+				let divContainer = document.createElement("div");
+
+				let image = document.createElement("img");
+				image.src = listing.data.thumbnail;
+
+
+				divContainer.append(image);
+
+				document.getElementById("result").append(divContainer);
+			})
+		})
+
+	} else {
+		
+		document.getElementById("no-query").textContent = "Nothing happened";
+	}
+})
