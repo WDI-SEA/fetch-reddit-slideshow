90efc39e8bcb090d8780ca6fb9f419f22cc71682
Bryan Brinson <bryan@beb.com>
2019-07-31 10:42:57 -0700

    some minor cleanup after in-class review. the basics are unchanged

--- js/main.js
+++ js/main.js
@@ -1,57 +1,80 @@
 // https://www.reddit.com/search.json?q=cats+nsfw:no
 
-const baseUrl = 'https://www.reddit.com/search.json?limit=100&q=nsfw:no+'; //just append search string to end.
+const BASEURL = 'https://www.reddit.com/search.json?limit=100&q=nsfw:no+'; //just append search string to end.
+const INTERVAL = 2500;
 
-let button = document.getElementById('search-button');
-button.addEventListener('click', (e) => {
+
+
+document.getElementById('search-button').addEventListener('click', (e) => {
   e.preventDefault();
   let searchString = document.getElementById('search-box').value;
-  let query = baseUrl + searchString;
-
+  let query = BASEURL + searchString;
 
   getData(query);
 
 });
 
 let getData = (query) => {
-  try {
-    fetch(query)
-      .then(function (response) {
-        if (!response.ok) {
-          throw new Error(`Eh? ${response.status}`);
+  fetch(query)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error(`Eh? ${response.status}`);
+      } else {
+        return response.json();
+      }
+    })
+    .then(function (response) {
+      let imgUrlArray = getImageUrls(response); // console.log(`imgUrlArray is: ${imgUrlArray}`);
+      let i = 1; // start at one so that left image isn't = -1 in the left image
+      setImage(imgUrlArray, i);
+      let interval = setInterval(function () {
+        if (i < imgUrlArray.length) {
+          setImage(imgUrlArray, i);
+          i++;
         } else {
-          return response.json();
+          i = 1;
         }
-      })
-      .then(function (response) {
-        let imgUrlArray = getImageUrls(response); // console.log(`imgUrlArray is: ${imgUrlArray}`);
-        let i = 1; // start at one so that left image isn't = -1 in the left image
+      }, INTERVAL);
 
-        let interval = setInterval(function () {
-          if (i < imgUrlArray.length) {
-            // should handle broken links, but it doesn't.
-            document.getElementById('center-image').src = `${imgUrlArray[i]}`;
-            document.getElementById('left-image').src = `${imgUrlArray[i-1]}`;
-            document.getElementById('right-image').src = `${imgUrlArray[i+1]}`;
+      document.getElementById('stop-button').addEventListener('click', (e) => {
+        clearInterval(interval);
+        document.getElementById('stop-button').style.display = 'none';
+        document.getElementById('image-wrapper').style.display = 'none';
+        document.getElementsByTagName('form')[0].style.display = 'unset';
+        document.getElementById('title').style.display = 'unset';
+      });
+      
+    })
+    .catch(function (err) {
+      console.log(`ERROR: ${err}`);
+    });
+};
 
-            document.getElementsByTagName('form')[0].style.display = 'none';
-            document.getElementById('image-wrapper').style.display = 'unset';
-    
-            i++;
-          } else {
-            i = 1;
-          }
-        }, 2000);
-      })
-  } catch (err) {
-    // the fetch above doesn't seem to trigger the catch.
-    console.log(`Caught error in try..catch:${err}`);
-    document.getElementsByTagName('form')[0].style.display = 'none';
-    document.getElementById('image-wrapper').style.display = 'unset';
-  }
+let setImage = function (imgUrlArray, i) {
+  // should handle broken links, but it doesn't.
+  document.getElementById('center-image').src = `${imgUrlArray[i]}`;
+  document.getElementById('left-image').src = `${imgUrlArray[i-1]}`;
+  document.getElementById('right-image').src = `${imgUrlArray[i+1]}`;
+
+  document.getElementsByTagName('form')[0].style.display = 'none';
+  document.getElementById('title').style.display = 'none';
+  document.getElementById('stop-button').style.display = 'unset';
+  document.getElementById('image-wrapper').style.display = 'unset';
 };
 
 let getImageUrls = function (d) {
+
+  // //incomplete in-clasee exempla using .map
+  // let a = d.map(function(p) {
+  //   return {
+  //     url: p.data.url,
+  //     post_hint: p.data.post_hint
+  //   }
+  // });
+  // console.log(a);
+
+
+  // my original extraction
   let a = [];
   d.data.children.forEach(e => {
     if (e.data.preview) {
