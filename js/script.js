b17c2fd1d113a05f6180ebd9ed914f4a6fec1245
essleeung <essleeung@gmail.com>
2020-03-27 02:32:14 -0400

    added some css transitions & styling. refactored code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Declare global variable and constants
 const API_URL_BASE = 'http://www.reddit.com/search.json?nsfw=no&q='
-const INTERVAL_DELAY = 2000
+const INTERVAL_DELAY = 3000
 let currentImages = []
 let currentIndex = 0
 let interval = null
@@ -10,30 +10,44 @@ let h3 = document.createElement('h3')
 document.getElementById('search-form').addEventListener('submit', (e) => {
     // Prevent the form's behavior of refreshing the page
     e.preventDefault()
+    
     // Grab the user's query
     let userQuery = document.getElementById('query').value
     // Make sure the user actually typed something
     if (userQuery) {
         // Good query, so perform the search
         fetchReddit(userQuery)
+        logoUp()
     }
     else {
         console.log('Um, please type something')
     }
 })
-// Stop button click
-document.getElementById('stop-button').addEventListener('click', () => {
-    console.log('STOP!!!')
+//function to make reddit logo go down & up
+const logoDown = () => {
+    document.getElementById('header').style.paddingTop = '35vh'
+}
+const logoUp = () => {
+    document.getElementById('header').style.paddingTop = '1vh'
+
+}
+
+const startSearch = () => {
+    logoDown()
     // Show the search bar
     document.getElementById('search-box').style.display = 'inline-block'
     //stop the interval 
     clearInterval(interval)
     // Hide the slideshow div
     document.getElementById('slideshow').style.display = 'none'
-    //clear the search input fiedl
+    //clear the search input field
     document.getElementById('query').value = ''
+    //reset curretIndex
+    currentIndex = 0;
+}
+//stop button event handler
+document.getElementById('stop-button').addEventListener('click', startSearch)
 
-})
 // Helper functions
 // The fetch function
 const fetchReddit = (userQuery) => {
@@ -59,7 +73,6 @@ const fetchReddit = (userQuery) => {
                     gold: item.data.gilded > 0
                 }
             })
-            console.log(results)
             //assign results back to global variable for accessing everywhere
             currentImages = results
             console.log('this is your array of current images:' + currentImages)
@@ -75,11 +88,11 @@ const startSlideshow = () => {
     // Hide the search bar
     document.getElementById('search-box').style.display = 'none'
     // Show the slideshow div
-    document.getElementById('slideshow').style.display = 'inline-block'
+    document.getElementById('slideshow').style.display = 'block'
     // Display the first image
     displayCurrent()
     // Kick off the interval
-    interval = setInterval(displayNext, 2000)
+    interval = setInterval(displayNext, 3000)
 }
 // Display current image
 const displayCurrent = () => {
@@ -87,35 +100,42 @@ const displayCurrent = () => {
     let img = document.createElement('img')
     img.src = currentImages[currentIndex].url
     img.alt = currentImages[currentIndex].title
-    img.setAttribute('id', 'img'+[currentIndex])
-
     // Create an H3 to hold the title
     let h3 = document.createElement('h3')
     h3.textContent = currentImages[currentIndex].title
-    h3.setAttribute('id', 'h3'+[currentIndex])
-    // Place my newly created elements into the DOM
-    document.getElementById('results').append(img)
-    document.getElementById('results').append(h3)
-}
-// Updates the next image
-const displayNext = () => {
-    // Empty previous images
-    document.getElementById('img'+[currentIndex]).remove()
-    document.getElementById('h3'+[currentIndex]).remove()
-    
-    // Create an image tag
-    let img = document.createElement('img')
-    
-    img.src = currentImages[currentIndex + 1].url
-    img.alt = currentImages[currentIndex + 1].title
-    img.setAttribute('id', 'img'+[currentIndex+1])
-    // Create an H3 to hold the title
-    let h3 = document.createElement('h3')
-    h3.textContent = currentImages[currentIndex + 1].title
-    h3.setAttribute('id', 'h3'+[currentIndex+1])
-
     // Place my newly created elements into the DOM
     document.getElementById('results').append(img)
     document.getElementById('results').append(h3)
     currentIndex++
 }
+// Updates the next image
+const displayNext = () => {
+    if (currentIndex < currentImages.length) {
+        // Empty previous images
+        document.getElementById('results').innerHTML = ""
+        displayCurrent()
+        console.log('current index' + currentIndex)
+    } else {
+        logoDown()
+        document.getElementById('look').textContent = "So sad, we ran out of pictures!"
+        document.getElementById('results').innerHTML = ""
+        document.getElementById('stop-button').remove()
+        //stop the interval 
+        clearInterval(interval)
+        restartSearch()
+
+
+    }
+}
+const restartSearch = () => {
+    //create a button to restart search
+    let tryAgain = document.createElement('button')
+    tryAgain.setAttribute('id', 'tryBtn')
+    tryAgain.innerHTML = 'Search again!'
+    document.getElementById('slideshow').append(tryAgain)
+    logoDown()
+    //append event listener to try again search button
+    document.getElementById('tryBtn').addEventListener('click', startSearch)
+    
+}
+
