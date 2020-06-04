e3f6addc0636fee88db3fb4b54d2a8b8f6a6bdaf
Louis <lreillywillhight@gmail.com>
2020-06-03 21:09:10 -0700

    backend update, defined function to produce a parsed list of jpg urls

--- js/script.js
+++ js/script.js
@@ -1,39 +1,68 @@
 console.log('loading')
 
-let homeUrl = "https://www.reddit.com/search.json?q=cats"
+let homeUrl = "https://www.reddit.com/search.json?q=pics"
+imageSource = document.getElementById('screen').src
 
 
 
 function getJson() {
   fetch(homeUrl)
-  .then(function (responseData) {
-    return responseData.json()
-  })
-  .then(function (jsonData) {
-    console.log(jsonData.data.children[0].data.url)
-  })
-  .catch(function (error) {
-    console.log('error')
-    console.log(error)
-  })
+    .then(function (responseData) {
+      return responseData.json()
+    })
+    .then(function (jsonData) {
+      console.log(jsonData.data.children[0].data.url)
+    })
+    .catch(function (error) {
+      console.log('error')
+      console.log(error)
+    })
 }
 
 //return all jsonData.data.children[i].data.url
 // let jurl = jsonData.data.children.data.url;
 
+//ISIMAGE returns true if a url is an image ending in (.jpg ...)
+let isImage = function (u) {
+  return u.includes('.jpg')
+}
+
+let urlList = [];
+
 function getJsonUrl() {
   fetch(homeUrl)
-  .then(function (responseData) {
-    return responseData.json()
-  })
-  .then(function (jsonData) {
-    let jurl = jsonData.data.children;
-    for (i = 0; i < jurl.length; i++) {
-      console.log(jurl[i].data.url)
+    .then(function (responseData) {
+      return responseData.json()
+    })
+    .then(function (jsonData) {
+      urlList = []
+      let jurl = jsonData.data.children;
+      for (i = 0; i < jurl.length; i++)
+        if (isImage(jurl[i].data.url)) {
+          urlList.push(jurl[i].data.url)
+        } else {
+          delete jurl[i].data.url
+        }
     }
-  })
+    )
 }
 
 
+// function getJsonUrl() {
+//   fetch(homeUrl)
+//     .then(function (responseData) {
+//       return responseData.json()
+//     })
+//     .then(function (jsonData) {
+//       let jurl = jsonData.data.children;
+//       for (i = 0; i < jurl.length; i++)
+//       if (isImage(jurl[i].data.url)) {
+//         console.log(jurl[i].data.url)
+//       } else {
+//         console.log('hmm')
+//       }
+//     })
+// }
 
-  console.log('loaded!!!')
+
+console.log('loaded!!!')
