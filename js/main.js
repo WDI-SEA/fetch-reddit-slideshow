68e0f24507c34d8e7f0a2ec223520d4d8b1a7138
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-06-04 01:41:07 -0500

    added callback functions and replaced for loop with map array iterator, debugged curly braces issue in JS and url

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 //DOM REFERENCES
 let goButton = document.getElementById('go');
 let stopButton = document.getElementById('stop');
-let inputF = document.getElementById('inputForms');
-let searchValue = document.getElementById('searchText');
+let searchValue = document.getElementById('searchText').value;
+var submitForm = document.getElementById('inputForm');
 // let imgIn = document.getElementById('slideshow');
  
 
 
 //testing url and fetch to find where pics are stored
-let url = "http://www.reddit.com/search.json?q=nature+nsfw:no";
+//  url = "http://www.reddit.com/search.json?q=nature+nsfw:no";
+var link = `http://www.reddit.com/search.json?q=${e.target[0].value}+nsfw:no`;
 
 
 
 //fetch function
-const getData = function() {
-    fetch(url)
+const getData = function(e) {
+    e.preventDefault();
+    getData(searchValue);
+    form.remove();
+
+    fetch(link)
         .then(function(responseData) {
             let jsonedData = responseData.json();
             return jsonedData;
@@ -25,33 +30,26 @@ const getData = function() {
             let findImage = jsonData.data.children
             let imageArr = [];
 
-            for (let i = 0; i < findImage.length; i++) {
-                let imageInsert = document.getElementById('slideshow')
-                let imgur = document.createElement("img");
-                imgur.src = `${findImage[i].data.url}`
-                imageArr.push(imgur.src);
-                //Question: If I push an image into an array(with its src), how can I view it?
-                // console.log(data[i].preview.images[source])
+            imageArr = findImage.map(function(imgLine) {
+                return imgLine.data.url
+            })
+            filterJPG = imageArr.filter(imgLine);
+            setInterval(slideImg, 5000);
             }
-        }) 
         .catch(function(error) {
             console.log('farts');
             console.log(error)
         })
+    }
+
+
+function imgLine(display) {
+    if (display.includes(".jpg")) {
+        return true
+    }
+}
+function slideImg () {
+
 }
 
-getData();
-
-//initialization function that clears out the form function
-
-function urlCom(url) {
-    let urlInsert = searchValue.value ;
-    url = "http://www.reddit.com/search.json?q=" +urlInsert+ "+nsfw:no";
-    //this function should concatenate the search value with the url
-}
-
-
-//click function that takes in data inputted and replaces with slideshow and stop button;
-goButton.addEventListener('click', getData);
-
-//click function that stops and returns the search bar
+form.addEventListener('submit', getData);
