c5baa16ed60426a3e6856778afb4b9e63ecc1fd6
zbarovsky <zachary.barovsky@gmail.com>
2020-06-06 15:01:44 -0700

    slides technically work. But have a low cap before images go away. Clear button works, still looks ugly

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 let stop = document.getElementById("stop");
 let search = document.getElementById("searchButton");
 let slides = document.getElementById("slideshowLocation");
+
 /* ------------- GAME LOGIC VARIABLES --------------------- */
 
 let results = [];
@@ -16,21 +17,31 @@ let titleArr = [];
 
 /* ----------------- READ DEEZ EVENTS --------------------- */
 search.addEventListener("click", createSlideshow);
-// slide.addEventListener("load", postSlides)
-
+stop.addEventListener("click", reset);
 
 
 /* ------------------------- Writting Dem Functions -------------------- */
 
 // takes user input and changes search parameters for slide show
 function createSlideshow(e) {
-    e.preventDefault()
-    let searchResult = document.getElementById("searchBar").value
-    url = `https://www.reddit.com/search.json?q=${searchResult}+nsfw:no`
-    slideshow()
+    e.preventDefault();
+    let searchResult = document.getElementById("searchBar").value;
+    url = `https://www.reddit.com/search.json?q=${searchResult}+nsfw:no`;
+    slideshow();
+    stop.disabled = false;
     //postSlides()
     //console.log(url)
 };
+
+function reset(e) {
+    e.preventDefault();
+    photoArr = [];
+    titleArr = [];
+    slides.src = "https://www.placecage.com/c/400/400";
+    url = "https://www.reddit.com/search.json?q=cats+nsfw:no";
+    clearInterval(interval);
+}
+
 
 /* -------------- get images from Reddit via JSON --------------------- */
 
@@ -49,22 +60,21 @@ fetch(url)
         for (let i = 0; i < results.length; i++) {
             if (results[i].data.url.endsWith("jpg")) {
                 photoArr.push(results[i].data.url);
+                //slides.src++
                 //console.log(results[0].data.url);
-                //console.log(photoArr[0])
+                //console.log(results[1])
             }
                 // titleArr.push(results[i].data.title)
                 // console.log(titleArr);
-  }
+    }
   function postSlides() {
-      for (let i = 0; i < photoArr.length; i++){
-        slides.src = photoArr[i];
-        console.log(slides.src)
-        //photoArr[i]++
-        //console.log(photoArr[i])
-        //console.log(photoArr.indexOf("https://i.redd.it/u53ct56yknz41.jpg"))
-      }
+        slides.src = photoArr.shift();
+        slides.src[0]++
+        //photoArr.fill(results)
+        //console.log(photoArr)
+        //console.log(slides.src[0])
 }
-setInterval(postSlides, 10000)
+interval = setInterval(postSlides, 3000)
 })
 .catch(errorCatcher);
 };
@@ -74,34 +84,40 @@ setInterval(postSlides, 10000)
 
 
 
-/*------------------------ dont fuck with this for now --------------------*/
-// fetch(url) 
-// .then(function(responseData) {
-//     return responseData.json();
-// })
-// .then(function(jsonData) {
-//     // console.log(jsonData.data.children);
-//     results = jsonData.data.children;
-//         for (let i = 0; i < results.length; i++) {
-//             if (results[i].data.url.endsWith("jpg")) {
-//                 photoArr.push(results[i].data.url);
-//                 //console.log(photoArr)
-//                 //console.log(photoArr.indexOf("https://i.redd.it/u53ct56yknz41.jpg"))
-//             }
-//                 // titleArr.push(results[i].data.title)
-//                 // console.log(titleArr);
-//   }
-//   function postSlides() {
-//       for (let i = 0; i < photoArr.length; i++){
-//         slides.src = photoArr[i];
-//         photoArr[i]++
-//         //console.log(photoArr[i])
-//         //console.log(photoArr.indexOf("https://i.redd.it/u53ct56yknz41.jpg"))
-//       }
-// }
-// setInterval(postSlides, 2000)
-// })
-// .catch(errorCatcher);
-// };
+/*------------------------ dont fuck with this for now Safety Net --------------------*/
 
-// });
+// function slideshow() {
+//     function errorCatcher(error) {
+//         console.log(error)
+//     };
+    
+//     fetch(url) 
+//     .then(function(responseData) {
+//         return responseData.json();
+//     })
+//     .then(function(jsonData) {
+//         //console.log(jsonData.data.children.0.url);
+//         results = jsonData.data.children;
+//             for (let i = 0; i < results.length; i++) {
+//                 if (results[i].data.url.endsWith("jpg")) {
+//                     photoArr.push(results[i].data.url);
+//                     //slides.src++
+//                     //console.log(results[0].data.url);
+//                     //console.log(photoArr[0])
+//                 }
+//                     // titleArr.push(results[i].data.title)
+//                     // console.log(titleArr);
+//         }
+//       function postSlides() {
+//             slides.src = photoArr.shift();
+//             slides.src[0]++
+//             // photoArr.fill(results[i].data.url)
+//             console.log(photoArr)
+//             console.log(slides.src[0])
+//     }
+//     setInterval(postSlides, 3000)
+//     })
+//     .catch(errorCatcher);
+//     };
+    
+//     });
