3122f14e93391c9da2cd2e045d9f908a043e9068
Eliott R Davidson <fussypuppet@Eliotts-MacBook-Pro.local>
2020-06-06 15:31:34 -0700

    Minimum functionality complete

--- js/app.js
+++ js/app.js
@@ -10,33 +10,43 @@ document.addEventListener('DOMContentLoaded', function() {
     searchButton.addEventListener("click", startShow);
     endButton.addEventListener("click", endShow);
 
-    console.log(searchButton);
+    const thesePhotoResults = [];
+    let photoInterval;
+
+    function getMorePhotos(searchTerm){
+        thisTheater.innerHTML = "Please wait.  Selecting only the choicest images takes time."
+        fetch(`https://www.reddit.com/search.json?limit=100&q=${searchTextField.value}`)
+        .then(function(responseData){
+            return responseData.json();
+        })
+        .then(function(jsonData){
+            let theseResultUrls = jsonData.data.children.map(function(child){
+                return child.data.url;
+            })
+            let thesePhotoResults = theseResultUrls.filter(function(thisResult){ return (thisResult.substring(thisResult.length-4, thisResult.length) === ".jpg")});
+            if (thesePhotoResults.length > 0){
+                console.log("I now have " + thesePhotoResults.length + " photos.  Displaying random results.");
+                photoInterval = setInterval(function() {
+                    let nextPhoto = thesePhotoResults[Math.floor(Math.random()*thesePhotoResults.length)];
+                    console.log("Next photo url is " + nextPhoto);
+                    thisTheater.innerHTML = `<img src="${nextPhoto}" alt="image search result">`;
+                }, 5000);
+            } else {
+                thisTheater.innerHTML = "Oh dear!  Not enough people post photos about this on Reddit.  It must not exist.";
+            }
+        })
+    }
 
     function endShow(){
-        thisTheater.style.opacity = "100%";;
+        clearInterval(photoInterval);
+        thisTheater.innerHTML = "<img src='thats-all-folks.jpg' alt='thats all folks!'>";
         endButton.style.display = "none";
         thisForm.style.display = "initial";
     }
 
     function startShow(e){
         e.preventDefault();
-        console.log(`search term was ${searchTextField.value}`);
-        fetch(`https://www.reddit.com/search.json?q=${searchTextField.value}`)
-            .then(function(responseData){
-                return responseData.json();
-            })
-            .then(function(jsonData){
-                console.log("Here is the data", jsonData);
-                let theseResultUrls = jsonData.data.children.map(function(child){
-                    return child.data.url;
-                })
-                let thesePhotoResults = theseResultUrls.filter(function(thisResult){ return (thisResult.substring(thisResult.length-4, thisResult.length) === ".jpg")});
-                console.log("thesePhotoResults:", thesePhotoResults);
-                if (thesePhotoResults.length > 0){
-                    thisTheater.innerHTML = `<img src="${thesePhotoResults[0]}" alt="image search result">`;
-                }
-            })
-        console.log("just fired JSON request");
+        getMorePhotos(searchTextField.value);
         endButton.style.display = "initial";
         thisForm.style.display = "none";
     }
