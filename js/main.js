058626c620ea7af16c8176ec3597625923b81ba9
Jameson Aardalen <jamesonaardalen@Jamesons-MBP.attlocal.net>
2020-06-08 09:24:58 -0700

    fixed bug

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ let searchBtn = document.getElementById("search-btn");
 let imgDiv = document.getElementById("img-div");
 let url = `http://www.reddit.com/search.json?q=${searchString}+nsfw:no`;
 let imgArr = [];
+let currentIndex = 0;
+let interval = null;
 
 // ---------- EVENT LISTENERS ----------
 searchBtn.addEventListener("click", searchImg);
@@ -16,14 +18,29 @@ function searchImg() {
       return res.json();
     })
     .then(function (data) {
-      console.log(data.data.children); // .children.data.preview
-      let imgArr = data.data.children;
-      let imgPath = imgArr[0].data.preview.images[0].source.url;
+      let imgsArr = data.data.children;
+      let imgSrcArray = imgsArr
+        .filter(function (img) {
+          if (img.data.preview == undefined) {
+            return false; //skip
+          } else {
+            return true;
+          }
+        })
+        .map(function (img) {
+          return img.data.preview.images[0].source.url;
+        });
+      console.log(imgSrcArray);
 
-      imgDiv.innerHTML = "";
-      imgDiv.innerHTML = `<img src="${imgPath}">`;
-      //   imgArr.forEach(function (img) {
-      //     console.log(img);
-      //   });
+      for (let i = 0; i < imgSrcArray.length; i++) {
+        setInterval(function () {
+          console.log(imgSrcArray[i]);
+          imgDiv.innerHTML = "";
+          imgDiv.innerHTML = `<img src="${imgSrcArray[i]}">`;
+        }, 5000);
+      }
+    })
+    .catch(function (err) {
+      console.log(err);
     });
 }
