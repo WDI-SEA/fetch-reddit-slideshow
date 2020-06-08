ea3903623d54517ff1a2a60b9faff37793765284
Paul Padian <paulpadian@Pauls-MacBook-Pro.local>
2020-06-08 10:01:12 -0600

    mirror not working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 let img1 = document.getElementById("01")
 let img2 =  document.getElementById("02")
 let img3 = document.getElementById("03")
+
+//--- mirror
+let img4= document.getElementById("04")
+let img5= document.getElementById("05")
+let img6= document.getElementById("06")
+
 let images = [];
 let jpegs = [];
 let url = "http://www.reddit.com/search.json?q="
@@ -36,11 +42,18 @@ function imageEditor(){
     } else {
         if(random2 === random3) {
             random3++
+        } else {
+            if(random == random3){
+                random3++
+            }
         }
     }
     img1.style.backgroundImage = 'url(' + jpegs[random] + ')'
+    // img4.style.backgroundImage = 'url(' + jpegs[random] + ')'
     img2.style.backgroundImage = 'url(' + jpegs[random2] + ')'
+    // img5.style.backgroundImage = 'url(' + jpegs[random2] + ')'
     img3.style.backgroundImage = 'url(' + jpegs[random3] + ')'
+    // img6.style.backgroundImage = 'url(' + jpegs[random3] + ')'
     
     console.log(jpegs)
 }
@@ -61,7 +74,7 @@ function submitMe() {
     url = "http://www.reddit.com/search.json?q="
     var value = document.getElementById('redditSearch').value;
     url += `${value}`;
-    url += `&limit=30&nsfw=no`
+    url += `&limit=40&nsfw=no`
     console.log(url)
     return fetcher(url);
    
