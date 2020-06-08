ca1ed1aa1235b9a21090cc297265364c3e89402d
Neri Ramirez <neriram@Neris-MacBook-Air.local>
2020-06-08 08:53:34 -0700

    slideshow final

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
 console.log('testing, 1,2,3');
+let reddit = 'https://reddit.com/search.json?q='
 
-function errorHandler(error) {
-    console.log('this is an error')
-}
+//event listeners for DOM elements 
+document.addEventListener('DOMContentLoaded', function() {
+   document.getElementById('search-bar').addEventListener('submit',redditData);
 
-fetch('https://reddit.com/search.json?q=cats%20and%20dogs%20cuddling')
-.then(function(responseData) {
-    let jsonData = responseData.json()
-    return responseData.json();
 })
-.then(function(jsonData) {
-    console.log(jsonData);
+function redditData(e) {
+    e.preventDefault()
+    let search = document.getElementById('search-bar').value;
 
-    //local variable 
-    let results = jsonData.data.children
-     //iterate through results
-     for (let i = 0; i < results.length; i++) {
-         console.log(results[i].data.title);
-         console.log(results[i].data.url)
-     }
-.catch(errorHandler)
+}
+fetch(reddit + search)
+    .then(function(responseData) {
+        let jsonData = responseData.json()
+        return responseData.json();
+    })
+    .then(function(jsonData) {
+        console.log(jsonData);
+        //posts array in the data
+        let results = jsonData.data.children
+        //iterate through results
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i].data.title);
+            console.log(results[i].data.url)
+        })
+    .catch(function(error) {
+        console.log("this is an error", error);
+    });
