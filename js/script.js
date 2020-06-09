67290474066762827283300ee94c134363855aa0
nguyendm77 <nguyendm77@gmail.com>
2020-06-09 08:42:22 -0700

    updating code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 
 document.addEventListener("DOMContentLoaded", function(){
     const btn = document.getElementById('btn');
-    const slideImg = document.getElementById('slideImg');
-    const searchInput =document.getElementById('input').value;
-    const url = `https://www.reddit.com/search.json?q=${searchInput}+nsfw:no`; 
+    const slideImg = document.getElementById('slideImg');  
     let searchResult = [];
     let searchImg = [];
-    btn.addEventListener('click', addImg(searchResult));
-    function addImg(searchResult) {
-        
+    btn.addEventListener('click', addImg);
+    function addImg() {
+        const searchInput =document.getElementById('input').value;
+        // console.log(searchInput);
+        const url = `https://www.reddit.com/search.json?q=${searchInput}+nsfw:no`; 
+        // console.log(url);
         fetch(url) 
             .then(function(responseData) {
                 return responseData.json();
             })
+            
             .then(function(jsonData) {
                 // console.log(jsonData);
     //  find result in jsonData
                 searchImg = jsonData.data.children;
-                 console.log(searchImg);
+                //  console.log(searchImg);
                 for (let i=0; i<searchImg.length; i++) {
                     if (searchImg[i].data.thumbnail.endsWith("jpg")) {
                         searchResult.push(searchImg[i].data.thumbnail);
@@ -28,7 +30,9 @@ document.addEventListener("DOMContentLoaded", function(){
                     let getImg = document.createElement("img");
                     getImg.setAttribute('src', searchResult[j]);
                     slideImg.appendChild(getImg);
+                    
                 }
+                setInterval(slideImg, 20);
             })
             .catch(function(error) {
                     console.log("Oh no, there's been an error!", error);
