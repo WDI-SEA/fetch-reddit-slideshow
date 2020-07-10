e6c0eb6386c5a0e592ae4e1e0ca290f00938949d
nessoaf <64707980+nessoaf@users.noreply.github.com>
2020-07-10 10:55:31 -0700

    apparently forgot to commit

--- js/js.js
+++ js/js.js
@@ -1,92 +1,90 @@
-console.log("Don't worry you're ok!")
-var images = []
-images[0] = "https://i.redd.it/168x8r17d0251.jpg"
+// Declare constants:
+const API_URL = 'https://www.reddit.com/search.json?nsfw=no&q='
+const INTERVAL_DELAY = 2500
+let currentImages = []
+let currentIndex = 0
+let interval = null
 
-let time = 0
-// i tapped out at 2.5 hours i freaking hate js
+document.getElementById('search-form').addEventListener('submit', e => {
+    e.preventDefault()
+    let userQuery = document.getElementById('query').value
 
+    if(userQuery){
+        fetchReddit(userQuery)
+        document.getElementById('query').value = ''
+    }
+    else{
+        console.log(`Hey bucko, that's an empty string.`)
+    }
+})
 
+document.getElementById('stop-button').addEventListener('click', () => {
+    document.getElementById('slideshow-container').style.visibility = 'hidden'
+    document.getElementById('form-container').style.display = 'block'
+    clearInterval(interval)
+})
 
-
-
-
-
-
-
-
-
-// timer function - for the picture totating
-// setInterval()
-function pictureChange() {  }
-  setInterval(function() {
-    console.log(time)
-    time++
-
-  }, 10000)
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// where to rip pictures
-fetch('https://www.reddit.com/search.json?q=nature')
-    .then(function(responseData){
-        return responseData.json()
+const fetchReddit = (query) => {
+    console.log('Fetching: ', query)
+    fetch(API_URL + query)
+    .then(response => response.json())
+    .then(jsonData => {
+        console.log(jsonData.data)
+        currentImages = jsonData.data.children.map(pic => {
+            return {
+                title: pic.data.title,
+                url: pic.data.url,
+                subreddit: pic.data.subreddit,
+                upvotes: pic.data.ups,
+                gold: pic.data.gilded > 0 ? true : false,
+                postHint: pic.data.post_hint
+            }
+        })
+        .filter(pic => {
+            return pic.postHint === 'image'
+        })
+        console.log('Cleaned up posts: ', currentImages)
+        startSlideshow()
     })
-    .then(function(jsonData) {
-        //do stuff wit data
-        console.log(jsonData);
-        //iterate through results
-let results = jsonData.data.children
-
-        for (let i =0; i < results.length; i++){
-        //console logf the title and url
-        console.log(jsonData.data.children[i].data.title)
-        console.log(jsonData.data.children[i].data.url)
-        }
+    .catch(err => {
+        console.log('ERROR: ' + err)
     })
-    .catch(function(error) {
-        console.log("💟")
-        console.log(error)
-    })  //primese syntax
-    console.log("finsihed the code")
+}
+
+const startSlideshow = () => {
+    currentIndex = 0
+
+    placeImage()
+    document.getElementById('form-container').style.display = 'none'
+    document.getElementById('slideshow-container').style.visibility = 'visible'
+    interval = setInterval(changeImage, INTERVAL_DELAY)
+}
+
+const changeImage = () => {
+    currentIndex ++
+
+    if(currentIndex >= currentImages.length){
+        currentIndex = 0
+    }
+
+    placeImage()
+}
+
+const placeImage = () => {
+    //Empty the result div, in case it has any content
+    document.getElementById('result').innerHTML = ''
+    // Create an img tag
+    let img = document.createElement('img')
+    img.src = currentImages[currentIndex].url
+    img.alt = currentImages[currentIndex].title
+    // Create an h2 tag to hold the image title
+    let h2 = document.createElement('h2')
+    h2.textContent = currentImages[currentIndex].title + (currentImages[currentIndex].gold? ' 🥇🏆' : '')
+    // Create an h3 tag to hold the subreddit name
+    let h3 = document.createElement('h3')
+    h3.textContent = currentImages[currentIndex].subreddit
+    // Add the created elements to my HTML page!
+    document.getElementById('result').append(img)
+    document.getElementById('result').append(h2)
+    document.getElementById('result').append(h3)
+}
