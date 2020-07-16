6963885bba57df1940c911ec50d844107996ae4b
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-07-15 20:01:29 -0700

    show image

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     let url = "http://www.reddit.com/search.json?q=cats+nsfw:no"
-
+    let INTERVAL_DELAY = 2500
+    let currentImages = []
+    let currentIndex = 0
+    let interval = null
 
     function errorHandler(error) {
         console.log(error);
     }
+
+    document.getElementById("input-container").addEventListener("submit", function(e) {
+        e.preventDefault();
+        let inputSearch = document.getElementById('input').value
+        if(inputSearch){
+            getImage(inputSearch)
+            document.getElementById('input').value = ''
+        }
+        else{
+            console.log(`That's an empty string.`)
+        }
+    })
 
     function getImage() {
         fetch(url)
@@ -23,25 +38,38 @@ document.addEventListener("DOMContentLoaded", function() {
                     return pic.postHint === "image"
                 })
                 console.log(currentImages)
-                // startSlideshow()
+                   startSlideshow()
             })
             .catch(errorHandler);
     }
     getImage();
-
-
-    document.getElementById("input-container").addEventListener("submit", function(e) {
-        e.preventDefault();
-        let inputSearch = document.getElementById('input').value
-        if(inputSearch){
-            getImage(inputSearch)
-            document.getElementById('input').value = ''
+    
+    function startSlideshow() {
+        currentIndex = 0
+        placeImage()
+        document.getElementById('input-container').style.display = 'none'
+        document.getElementById('slideshow').style.visibility = 'visible'
+        interval = setInterval(changeImage, INTERVAL_DELAY)
+    }
+    function changeImage() {
+        currentIndex ++
+        if(currentIndex >= currentImages.length){
+            currentIndex = 0
         }
-        else{
-            console.log(`That's an empty string.`)
-        }
-    })
+        placeImage()
+    }
+    function placeImage() {
+        document.getElementById('slideshow').innerHTML = ''
+        let img = document.createElement('img')
+        img.src = currentImages[currentIndex].url
+        img.alt = currentImages[currentIndex].title
+        let h2 = document.createElement('h2')
+        h2.textContent = currentImages[currentIndex].title + (currentImages[currentIndex].gold? '' : '')
+        let h3 = document.createElement('h3')
+        h3.textContent = currentImages[currentIndex].subreddit
+        document.getElementById('slideshow').append(img)
+        document.getElementById('slideshow').append(h2)
+        document.getElementById('slideshow').append(h3)
+    }
 })
-
-
 
