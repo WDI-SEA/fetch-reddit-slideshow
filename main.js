b1d877ff6967944165c5ba23cf6e6a643772bfa8
damatoaj <mr.arthurdamato@gmail.com>
2020-12-22 08:48:02 -0800

    second

--- main.js
+++ main.js
@@ -11,50 +11,49 @@ let makeFetchHappen = () => {
     fetch(`http://www.reddit.com/search.json?q=${input}+nsfw:no`)
     .then(response => response.json())
     .then((data) => {
-        
+
         let picture = data.data
+        let img
+        let urls = []
         console.log(picture)
         for (let i=0; i< picture.children.length; i++) {
-            let img = document.createElement("img")
+            img = document.createElement("img")
             img.src = picture.children[i].data.url;
-            document.getElementById('slides').append(img)
+            urls.push(img)
             // slide.appendChild(src)
             // console.log(slide)
+            
         } 
-
+        
         function changeImg() {
-            if(i < picture.children.length - 1) {
+            for (let i = 0; i < urls.length; i++) {
+                img = document.createElement("img")
+                document.getElementById('slides').append(urls[0])
+
+            // img.src = picture.children[i].data.url;
+
+            if(i < urls.length - 1) {
                 i++;
             } else {
                 i = 0;
             }
+        }   
         }
        
         setTimeout(changeImg(), time);
-        
+        // window.onload = changeImg();
     })
     
 }
 
-
+// window.onload = changeImg();
 
 
 //a button that clicks for starting the slideshow
 
 document.getElementById('start').addEventListener("click", makeFetchHappen)
 
-//make a timed slideshow
-// var i = 0;
-// var images = [];
-// var time = 3000;
-
-// //image list
-
-// //change img
-// function changeImg() {
-//     document.img.src = picture.children[i].data.url;
-
-//     if ()
+// //clear input field and div
+// function stopAll() {
+// document.getElementById('stop').reset();
 // }
-
-//clear input field and div
