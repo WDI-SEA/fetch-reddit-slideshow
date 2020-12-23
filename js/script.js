17bae7f835ab2303b0f2dee0fa3f0475dac2dea8
sameghosts <seanfeiner.brown@gmail.com>
2020-12-23 10:14:04 -0800

    used the basis of the solution to create something i can edit and restyle refactored gavins lesson and the solution almost as a code along

--- js/script.js
+++ js/script.js
@@ -1,69 +1,99 @@
 //Declaration of variables
-const API_URL = 'https://www.reddit.com/search.json?limit=15&q=';
-const INTERVAL_DELAY = 2000;
+const API_URL = 'https://www.reddit.com/search.json?nsfw=no&q='
+const INTERVAL_DELAY = 2000
 
-let currentImages = [];
-let currentIndex = 0;
-let interval;
+let currentImages = []
+let currentIndex = 0
+let interval = null
 
+// event listeners
+document.getElementById('search-form').addEventListener('submit', e => {
+    e.preventDefault()
+    //getting the user input from the submit box
 
+    let userQuery = document.getElementById('query').value
 
+    if (userQuery) {
+        fetchFromReddit(userQuery)
+        document.getElementById('query').value = ''
+    }else{
+        console.log('Empty String')
+    }
+})
 
+document.getElementById('stop-button').addEventListener('click', () =>{
+    document.getElementById('slideshow-container').style.visibility = 'hidden'
+    document.getElementById('form-container').style.display = 'block'
+    
+    clearInterval(interval)
+})
 
     // call the fetch arguments with
 // the then statements
-const fetchFromReddit = (e) => {
-    e.preventDefault();
+const fetchFromReddit = query => {
+    //preform the fetch 
+    console.log('doing the fetch', query)
+    fetch(API_URL + query)
+    .then(response => response.json())
+    .then(jsonData =>{
+        // consolidate data into only what we need / care about
+        currentImages = jsonData.data.children.map(p => {
+            return {
+            title: p.data.title,
+            url: p.data.url,
+            subreddit: p.data.subreddit,
+            posthint: p.data.post_hint
+        }
+    }).filter(p => {
+        return p.posthint === 'image'
+    })
+    console.log('cleaned up posts', currentImages)
+    startSlides()
+    })
+        .catch(err => {
+            console.log('Error', err)    
+    })
+}
 
-    let query = document.getElementbyId('query').value;
+const startSlides = () => {
+    console.log('slides a sliding')
+    //set image array index to first image
+    currentIndex = 0;
 
-    if (query){
-        fetch(API_URL + query)
-        .then(res => {
-            res.json()
-        
-        .then(result => {
-            let results = result.data.children
-           console.log(results);
-           currentImages = results.map(post => {
-               return {
-                   subreddit: post.data.subreddit,
-                   title: post.data.title,
-                   url: post.data.url.replace('.gifv', '.gif')
-               };
-           })
-                .filter(item => {
-                    return item.url.includes('i.imgur') || item.url.includes('i.redd');
-                })
-                currentIndex = 0;
+    placeImage()
+    
+    document.getElementById('form-container').style.display = 'none'
+    document.getElementById('slideshow-container').style.visibility = 'visible'
 
-                loadImage();
-                
-                clearInterval(interval);
+    interval = setInterval(changeImage, INTERVAL_DELAY)
 
-                interval = setInterval(changeSlide, INTERVAL_DELAY);
-            })
-            .catch(err => {
-                console.log('error', err);
-            }) 
-        })
-    } else {
-        console.log('Nothing to search! Womp Womp!!!');
+    const changeImage = () => {
+        //changes index interval by one
+        currentIndex++
+    
+        if(currentIndex >= currentImages.length){
+            currentIndex = 0
+        }
+
+        placeImage()
     }
-};
-document.getElementById("search-form").addEventListener("submit")
-)
-document.getElementById('search-form').addEventListener('submit', fetchFromReddit);
+    const placeImage = () => {
+        document.getElementById('result').innerHTML = ''
 
-const loadImage = () => {
-    let slideshow = document.getElementById('slideshow');
-    slideshow.innerHTML = '';
-    let newImg = document.createElement('img');
-    newImg.src = currentImages.url;
+        //create image tags in the document
+        let img = document.createElement('img')
+        img.src = currentImages[currentIndex].url
+        img.alt = currentImages[currentIndex].title
 
-};
+        let h2 = document.createElement('h2')
+        h2.textContent = currentImages[currentIndex].title
 
-//in order to search and return assetts 
-
-//MAYBE?? - extra RESET 
-//WebGL libraries????
+        let h3 = document.createElement('h3')
+        h3.textContent = 'r/' + currentImages[currentIndex].subreddit
+        h3.style.fontFamily = 'Helvetica'
+        
+        document.getElementById('result').append(img)
+        document.getElementById('result').append(h2)
+        document.getElementById('result').append(h3)
+    }
+}
