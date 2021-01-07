6ac46c1c7bb03928db42bc5aef184bd7fe727222
Blake DeGraw <bbdegraw@gmail.com>
2021-01-06 19:43:02 -0800

    routine commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     console.log('DOM Content Loaded')
 })
+
+const redditEndpoint = 'https://www.reddit.com/search.json?q='
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    console.log('form submitted')
+
+    fetch(redditEndpoint + e.target.searchBox.value)
+    .then((fetchedPics) => {
+        return fetchedPics.json()
+    })
+    .then((jsonPics) => {
+        console.log(jsonPics.data.children[0].data.preview.images[0].source.url)
+    })
+    .catch((error)=>{
+        console.log('Failed to fetch pics')
+    })
+})
