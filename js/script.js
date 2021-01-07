1979cc8a3440241719755b5994d6f671362032ec
tkadima <kadima.t@husky.neu.edu>
2021-01-07 12:50:24 -0500

    add more but nothing happened

--- js/script.js
+++ js/script.js
@@ -10,17 +10,24 @@ const redditEndpoint = 'https://www.reddit.com/search.json?q=kittens'
 // search images of what they are searching for should appear.
 
 // got it to fetch the ojbect now to put it on screen
-fetch(redditEndpoint)
-.then((fetchObj)=>{
-    console.log('here is the fetch object', fetchObj)
-    return fetchObj.json()
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    form.addEventListener('submit', (e)=> {
+        
+        fetch(redditEndpoint)
+        .then((fetchObj)=>{
+            console.log('here is the fetch object', fetchObj)
+            return fetchObj.json()
+        })
+        .then((jsonData)=>{
+            console.log('Here is the Json data', jsonData)
+        })
+        .catch((error =>{
+            console.log('oh no you did NOt make fetch happen')
+        }))
+    })
 })
-.then((jsonData)=>{
-    console.log('Here is the Json data', jsonData)
-})
-.catch((error =>{
-    console.log('oh no you did NOt make fetch happen')
-}))
+
 
 
 // create event listener to listen for a submit event
