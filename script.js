28161552881ff3dd71e3c7c9a4cbae7e556a8f7f
Blake DeGraw <bbdegraw@gmail.com>
2021-01-07 09:35:44 -0800

    slide show working better

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const redditEndpoint = 'https://www.reddit.com/search.json?q='
 let picURLS = []
-let myImage = null
-i = 0
-let displayPic = document.getElementById('displayPic')
+let i = 0
+let count = null
+
+
+function slideShow() {
+    displayPic.src = picURLS[i]
+    i++
+}
+
+function beginSlideShow() {
+    count = setInterval(slideShow, 2000)
+    slideShow()
+}
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
@@ -19,31 +30,7 @@ form.addEventListener('submit', (event) => {
             picURLS = jsonPics.data.children.map((result) => {
                 return result.data.url
             })
-            displayPic.src = picURLS[0]
-            sleep(2000).then(() => {displayPic.src = picURLS[1]})
-            sleep(4000).then(() => {displayPic.src = picURLS[2]})
-            sleep(6000).then(() => {displayPic.src = picURLS[3]})
-            sleep(8000).then(() => {displayPic.src = picURLS[4]})
-            sleep(10000).then(() => {displayPic.src = picURLS[5]})
-            sleep(12000).then(() => {displayPic.src = picURLS[6]})
-            sleep(14000).then(() => {displayPic.src = picURLS[7]})
-            sleep(1600).then(() => {displayPic.src = picURLS[8]})
-            sleep(18000).then(() => {displayPic.src = picURLS[9]})
-            sleep(20000).then(() => {displayPic.src = picURLS[10]})
-            sleep(22000).then(() => {displayPic.src = picURLS[11]})
-            sleep(24000).then(() => {displayPic.src = picURLS[12]})
-            sleep(26000).then(() => {displayPic.src = picURLS[13]})
-            sleep(28000).then(() => {displayPic.src = picURLS[14]})
-            sleep(30000).then(() => {displayPic.src = picURLS[15]})
-            sleep(32000).then(() => {displayPic.src = picURLS[16]})
-            sleep(34000).then(() => {displayPic.src = picURLS[17]})
-            sleep(36000).then(() => {displayPic.src = picURLS[18]})
-            sleep(38000).then(() => {displayPic.src = picURLS[19]})
-            sleep(40000).then(() => {displayPic.src = picURLS[20]})
-            sleep(42000).then(() => {displayPic.src = picURLS[21]})
-            sleep(44000).then(() => {displayPic.src = picURLS[22]})
-            sleep(46000).then(() => {displayPic.src = picURLS[23]})
-            sleep(48000).then(() => {displayPic.src = picURLS[24]})
+            beginSlideShow()
         })
         .catch((error) => {
             console.log('Failed to fetch pics')
