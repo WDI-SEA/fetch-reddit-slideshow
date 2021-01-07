868ee37cf989457e2a4471b44e58f76bb33a256a
Kenny Jean <kennyjean561@gmail.com>
2021-01-07 10:56:56 -0600

    deleted start btn

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 console.log("😎 Cool loading");
+
+
+const randomUserEndPoint = 'https://randomuser.me/api/?results='
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    form.addEventListener('submit', (e)=>{
+        e.preventDefault()
+        // stops all default functionality like page reloading when we submit a form
+        while(peopleList.firstChild) {
+            peopleList.removeChild(peopleList.firstChild)
+        }
+        
+
+
+        fetch(randomUserEndPoint+input.value)
+        .then((fetchedUsers)=>{
+            return fetchedUsers.json()
+        })
+        .then((jsonUsers)=>{
+            jsonUsers.results.forEach(addPerson)
+            
+        })
+        .catch((err)=>{
+            console.log('Failed to fetch Users!!', err)
+        })
+        
+    })
+})
