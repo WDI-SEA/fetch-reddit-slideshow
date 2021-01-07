16064b86803dcb9736a0ab029c183749d5e4c6ba
ElyssaW <ejwinch1@gmail.com>
2021-01-07 00:08:39 -0600

    The code now does everything the deliverable is supposed to do. Technically. Looks awful, though. And occasionally throws up dead images.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('console running')
     
     let imgUrl
+    let timer
     
     function deleteChildren() {
         while (imageBank.firstChild) {
@@ -9,11 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    let requestURL = 'https://www.reddit.com/search.json?q=kittens'
+    let requestURL
 
     document.addEventListener('submit', (e) => {
         e.preventDefault()
-        requestURL = 'https://www.reddit.com/search.json?q=kittens'
+        desc.style.visibility = 'hidden'
+        pause.style.visibility = 'visible'
+        requestURL = 'https://www.reddit.com/search.json?q=' + input.value
         
          // Promise statement - resolve this fetch statement
         fetch(requestURL)
@@ -25,8 +28,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // This catches the data returned from the previous .then, populating the jsonData variable
         .then((jsonData) => {
-            console.log(jsonData.data.children[0].data.thumbnail)
-            display.src = jsonData.data.children[0].data.thumbnail
+            let index = 0
+            
+            timer = setInterval(() => {
+                imgUrl = jsonData.data.children[index].data.thumbnail
+                
+                if (index === jsonData.data.children.length) {
+                    index = 0
+                } else {
+                    index++
+                }
+                
+                console.log(jsonData.data.children[index])
+                display.src = jsonData.data.children[index].data.thumbnail
+            }, 2000)
         })
 
         // Throw an error if the promise is not fulfilled
@@ -34,5 +49,9 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('error!')
             console.log(error)
         })
+        
+        pause.onclick = () => {
+            clearInterval(timer)
+        }
     })
 })
