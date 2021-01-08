6f9680d0b6e4bca0350f8c8a68ac3fea1e0268f5
Your Name <you@example.com>
2021-01-08 11:35:15 -0800

    Refactored code to use string interpolation. Also put an arrow function on one line to illustrate how doing so means you don't need the curly braces.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
-// How do I get more than 25 results? My objects length is always 25, I can't get it to be more than 25.
-
-const redditEndpointFirst = 'http://www.reddit.com/search.json?q=nsfw:no+'
-const redditEndpointSecond = '&limit=100'
 let imageURLs = []
 let i = 0 // Index for iterating through imageURLs.
 
 // imageInterval needs to be global so that I can 'pause' it from outside my 'start' function.
 let imageInterval
 
-// Figure out whether or not each search result has an image. This is where I'll need to find the exact path of my JSON objects.
+// Figure out whether or not each search result has an image. 
+// If it does, grab that url and add it to my array of img URLs.
 const grabImageURL = (jsonObject, index) => { // We don't use index but want to write it out to remind myself that forEach also gives the index. 
     // jsonObject.data.children[].data.url_overridden_by_dest
-    let title = jsonObject.data.title
-    let subreddit = jsonObject.data.subreddit
     let url = jsonObject.data.url_overridden_by_dest
 
     if (url) { // Not all objects have this key value. For example, text posts don't have this attribute.
@@ -24,20 +19,18 @@ const grabImageURL = (jsonObject, index) => { // We don't use index but want to 
 
 // Grab results
 const grabResults = (searchTerm) => {
-    fetch(redditEndpointFirst + searchTerm + redditEndpointSecond)
+    fetch(`http://www.reddit.com/search.json?q=nsfw:no+${searchTerm}&limit=100`)
     .then((fetchedResults) => {
         console.log('This delay tells us when we hear back from Reddit') // Note: This takes a while to show up. 
         return fetchedResults.json()
     })
     .then((jsonObjects) => {
         jsonObjects.data.children.forEach(grabImageURL) 
-        // We can only start doing this once we have values in imageURLs.
+        // We can only start doing the below once we have values in imageURLs.
         changeImage()
         imageInterval = setInterval(changeImage, 3000) 
     })
-    .catch((error) => {
-        console.log('Like, stop trying to make fetch happen!', error)
-    })
+    .catch((error) => console.log('Like, stop trying to make fetch happen!', error))
 }
 
 form_box.addEventListener('submit', (event) => {
