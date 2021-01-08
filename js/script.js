f05f0e84b39c8ce7f4a1df7dc92f93dd67b2a279
Kenny Jean <kennyjean561@gmail.com>
2021-01-08 16:58:26 -0600

    assistance

--- js/script.js
+++ js/script.js
@@ -1,29 +1,67 @@
-console.log("😎 Cool loading");
+
+console.log("😎 Cool runnings🛷");
 
 
-const redditPicEndPoint = 'https://www.reddit.com/search.json?q=kittens'
-
-document.addEventListener('DOMContentLoaded', ()=> {
-    form.addEventListener('submit', (e)=>{
-        e.preventDefault()
-        // stops all default functionality like page reloading when we submit a form
-        // while(peopleList.firstChild) {
-        //     peopleList.removeChild(peopleList.firstChild)
-        // }
-        
+const form = document.getElementById('form');
+const display = document.getElementById('display')
+const stopBtn = document.getElementById('stop')
+let slideshow = [];
 
 
-        fetch(redditPicEndPoint + input.value)
-        .then((fetchedPics)=>{
-            return fetchedPics.json()
-        })
-        .then((jsonPics)=>{
-            jsonPics.results.forEach(addPerson)
+
+
+
+
+//*********** reddit slideshow homework
+
+// const redditPicEndPoint = 'https://www.reddit.com/search.json?q=kittens'
+
+// fetch(redditPicEndPoint)
+//         .then( function(fetchedPics) {
+//             return fetchedPics.json();
+//         })
+//         .then(function(jsonPics) {
+//             console.log('Pictures loaded');
             
-        })
-        .catch((err)=>{
-            console.log('Failed to fetch Users!!', err)
-        })
+//         })
+//         .catch(function(err) {
+//             console.log('Failed to fetch Pics!!', err)
+//         })
+
+//         console.log('Just fired AJAX Request');
+
+
+
+
+// const addPictures = (pictures) =>{
+//     // let peopleList = document.getElementById('peopleList');
+//     let newPerson = document.createElement('li');
+//     newPerson.textContent = `${person.name.first} ${person.name.last}`
+//     peopleList.appendChild(newPerson) 
+//     // append info in child we just created
+// }
+// document.addEventListener('DOMContentLoaded', ()=> {
+//     form.addEventListener('submit', (event)=>{
+//         event.preventDefault()
+//         // stops all default functionality like page reloading when we submit a form
+//         // while(peopleList.firstChild) {
+//         //     peopleList.removeChild(peopleList.firstChild)
+//         // }
         
-    })
-})
+
+
+//         fetch(redditPicEndPoint)
+//         .then((fetchedPics)=>{
+//             return fetchedPics.json()
+//         })
+//         .then((jsonPics)=>{
+//             // jsonPics.results.forEach(addPerson)
+//             jsonPics.results.forEach(addPictures)
+            
+//         })
+//         .catch((err)=>{
+//             console.log('Failed to fetch Pics!!', err)
+//         })
+        
+//     })
+// })
