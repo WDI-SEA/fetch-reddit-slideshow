059ba9d82b8b5665f4f36ec763e4d05633a987e6
tkadima <kadima.t@husky.neu.edu>
2021-01-09 16:44:08 -0500

    cleaned up syntax a bit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-// create a redditEndPoint for some types of searches you want on site
 console.log('what up')
 const stopBtn = document.getElementById('stop')
 let images = []
@@ -7,15 +6,7 @@ const inputBox = document.getElementById('input')
 const picTube = document.getElementById('screen')
 let slideIndex = 0
 let slides;
-// console.log(inputBox.value)
-// const randomRedditEndpoint = 'https://reddit.com/search.jsonq?=dogs'
-// const randomRedditEndpoint = 'https://reddit.com/search.jsonq?=birds'
-// create lists in JS for the images to be put in
 
-// when user types what they want to search in the input bar and click
-// search images of what they are searching for should appear.
-
-// got it to fetch the ojbect now to put it on screen
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -58,15 +49,6 @@ stopBtn.addEventListener('click', () => {
     stopSlideShow()
 }) 
 
-// const doSomething = (pizza) => {
-//     pizza.preventDefault()
-//     console.log('pizza the hut')
-// }
 
-// form.addEventListener('submit',(pizza) => {
-//     pizza.preventDefault()
-//     console.log('pizza the hut')
-// } )
 
-// create event listener to listen for a submit event
 
