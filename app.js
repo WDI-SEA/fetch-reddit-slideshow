b7b0e7b718588cb9506b669d7fb2471a9745954b
edgard Jara <edv06jara@gmail.com>
2021-01-15 16:31:26 -0800

    almost done

---
+++ app.js
@@ -0,0 +1,87 @@
+let fotoIndex = 0; 
+let btn = document.getElementById("btn");
+let imgReturn = document.getElementById("slide");
+let input = document.querySelector("input");
+let fotos = []
+
+btn.addEventListener("click", function(e){
+    e.preventDefault();
+    console.log("text")
+fetch('http://www.reddit.com/search.json?q='+input.value+'+nsfw:no')
+        .then(function(responseBtn){
+            return responseBtn.json();
+        })
+        .then(function(jsonData){
+            // imgReturn.innerText=jsonData.wolfPic
+            console.log(jsonData.data.children)
+            let image = jsonData.data.children.forEach(img => {
+                // show if an image is a url string or default or self 
+                if (img.data.thumbnail != "default" &&  img.data.thumbnail != "self") {
+                    fotos.push(img.data.thumbnail)
+                    
+                }
+
+            })
+            console.log(fotos)
+        })
+        .then(() => {
+            
+            setInterval(() => {
+                imgReturn.src = fotos[fotoIndex]
+                imgReturn.style.display = "block";
+                if(fotoIndex < fotos.length-1){
+                    fotoIndex++
+                } else {
+                    fotoIndex = 0
+                }
+            }, 2000)
+        })
+        
+})
+// now render and image and specify the width and height too
+
+
+function slideShow () {
+    imgReturn.src = fotos[1];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const API_URL= 'https://www.reddit.com/search.json?limit=15&q=';
+// const INTERVAL_DELAY = 2000;
+ 
+// let currentImages =[];
+//  let currentIndex =0;
+//  let interval;
+ 
+//  const fecthFromReddit = e => {
+//      e.preventDefault();
+
+//      let query = document.getElementById('query').value;
+//      if (query) {
+//          fecth()
+//      }
+//  }
+// const imgae
+
+
+
+
+
+
+
+
