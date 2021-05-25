eff36f0240eac352c6e2fd9049cd28bffc5bf4bc
funbunch <shannan_bunch@hotmail.com>
2021-05-24 23:46:25 -0700

    Cleaned up fetchData function. added tailwind styles

--- js/app.js
+++ js/app.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let inputForm = document.querySelector('form')
     let displayImg = document.querySelector('display-img')
     let imgResponse = []
-  
+
+    inputForm.addEventListener("submit", fetchData)
+    
     // REQUEST DATA
+    function fetchData(e) {
     // take form element and prevent default behavior
-    inputForm.addEventListener("submit", (e) => {
+    // inputForm.addEventListener("submit", (e) => {
         //Prevent refresh
         e.preventDefault()
         inputForm.style.display = 'none'
@@ -19,39 +22,39 @@ document.addEventListener("DOMContentLoaded", () => {
   
         // make fetch request to const api url with given user number
         fetch(requestUrl + userInput + '+nsfw:no')
-            // .then --> take response data and format
-            .then((res) => {
-                return res.json()
-            })
-            // .then --> use response JSON data
-            .then((jsonData) => {
-              imgResponse = jsonData.data.children
-              filterdImgs(imgResponse)
-              
-              // imgResponse = map(imgResponse => imgResponse.data.url_overridden_by_dest)
-              // // .filter(imgType => imgType === '.jpg' || imgType === '.png')
-              // if (imgResponse.endsWith('.jpg') || imgResponse.endsWith('.png')) {
-              //   console.log(imgResponse)
-              //   return imgResponse
-              // }
-              
-            })
-            // .catch --> catch errors
-            .catch((err) => {
-                console.log(err)
-                return err
-            })
-    })
-  
-    function filterdImgs(imgResponse) {
-      imgResponse = map(imgResponse => imgResponse.data.url_overridden_by_dest)
-      // .filter(imgType => imgType === '.jpg' || imgType === '.png')
-      if (imgResponse.endsWith('.jpg') || imgResponse.endsWith('.png')) {
-        console.log(imgResponse)
-        return imgResponse
+        // .then --> take response data and format
+        .then((res) => {
+            return res.json()
+        })
+        // .then --> use response JSON data
+        .then((jsonData) => {
+            imgResponse = jsonData.data.children
+            
+            .map(imgResponse => imgResponse.data.url_overridden_by_dest)
+            // .filter(imgType => imgType === '.jpg' || imgType === '.png')
+            // if (imgResponse.endsWith('.jpg') || imgResponse.endsWith('.png')) {
+            // console.log(imgResponse)
+            // return imgResponse
+            // }
+            console.log(imgResponse)
+            
+        })
+        // .catch --> catch errors
+        .catch((err) => {
+            console.log(err)
+            return err
+        })
+    // })   
     }
-  }
-          //show display show
+        //tried to functionify but didnt work        
+        // function filterdImgs(imgResponse) {
+        //     imgResponse = map(imgResponse => imgResponse.data.url_overridden_by_dest)
+        //     // .filter(imgType => imgType === '.jpg' || imgType === '.png')
+        //     if (imgResponse.endsWith('.jpg') || imgResponse.endsWith('.png')) {
+        //       console.log(imgResponse)
+        //       return imgResponse
+        //   }
+        // }
 
 
       // async function searchReddit() {
