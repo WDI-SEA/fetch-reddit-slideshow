88eea97e63b2dfbdc48decc8055b4a310a1d5ad7
Benji <bwolfenberger@gmail.com>
2021-05-24 23:45:52 -0700

    added restart button

--- js/script.js
+++ js/script.js
@@ -5,37 +5,50 @@ document.addEventListener('DOMContentLoaded', () => {
     const requestUrl = 'http://www.reddit.com/search.json?q='
     let counter = 0
     let searchResults = []
+    let interval
     let inputForm = document.querySelector('form')
-    let userInput
     document.getElementById('button').addEventListener('click', startFetch)
+    document.getElementById('restartButton').addEventListener('click', resetFetch)
     
-
-    inputForm.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let userInput = input.value
-        console.log(userInput)
-    })
-
+    
     function startFetch() {
-        console.log('button')
-        fetch(requestUrl + userInput)
-        .then((res) => {
-            return res.json()
-        })
-        .then((jsonData) => {
-            searchResults = jsonData.data.children.filter(allLinks => String(allLinks.data.url).includes('i.redd')).map(filteredImages => filteredImages.data.url)
-            changeImage()
-            // console.log(searchResults)
-        })
-        .catch((err) => {
-            return err
-        })
-        
-        function changeImage() {
-            newImage.src = searchResults[counter]
-            counter++
-        }
+        document.querySelector('h1').style.display = 'none'
 
-        setInterval(changeImage, 10000)
+        inputForm.addEventListener('submit', (e) => {
+            e.preventDefault()
+            let userInput = input.value
+            console.log(userInput)
+        
+            fetch(requestUrl + userInput)
+            .then((res) => {
+                return res.json()
+                console.log(requestUrl + userInput)
+            })
+            .then((jsonData) => {
+                searchResults = jsonData.data.children.filter(allLinks => String(allLinks.data.url).includes('i.redd')).map(filteredImages => filteredImages.data.url)
+                changeImage()
+                // console.log(searchResults)
+            })
+            .catch((err) => {
+                return err
+            })
+            
+            function changeImage() {
+                newImage.src = searchResults[counter]
+                counter++
+                if (counter === 10) {
+                    counter = 0
+                }
+            }
+            
+            interval = setInterval(changeImage, 1000)
+        })
+    
+    }
+
+    function resetFetch() {
+        clearInterval(interval)
+        searchResults = []
+        counter = 0
     }
 })
