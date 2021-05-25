bf6627e71504dbff5242974284c4088c9eed8161
Brian Young <berkshirehunt@gmail.com>
2021-05-24 20:20:42 -0700

    Drawing pictures, and only of a certin extension, jpg etc

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const requestURL = 'http://www.reddit.com/search.json?q='
   let inputForm = document.querySelector('#form')
   // let peopleList = document.querySelector('#peopleList')
-  // let picRes = []
+  let picRes = []
 
   // REQUEST DATA
   // Take form element and prevent default behavior
@@ -16,22 +16,40 @@ document.addEventListener('DOMContentLoaded', () => {
     // Get user inputted number
     let userInput = input.value
     redString = `${requestURL}${userInput}+nsfw:no`
+    console.log(redString)
 
     //Make FETCH request to const API URL with given user number
     fetch(redString)
       // .then --> take response data and format
-      .then((redString) => {
+      .then((res) => {
         // console.log(redString)
         console.log('response came back')
-        return redString.json()
-        console.log(redString.json)
+        return res.json()
+        console.log(redString)
       })
       .then((jsonData) => {
-        picRes = jsonData.results
-        console.log(picRes)
+        picRes = jsonData.data.children
         // console.log(picRes)
-        // console.log(jsonData)
-        // domPeopleList(peopleRes)
+        for (let i = 0; i < picRes.length; i++) {
+          //   if (results[i].data.url.endsWith(".jpeg") || results[i].data.url.endsWith(".png") || results[i].data.url.endsWith(".jpg")){
+          //     imgArray.push(results[i].data.url);
+          // } else {
+          //     console.log("Uh oh! Nothing to see here. Sorry!");
+          // }
+
+          if (
+            picRes[i].data.url.endsWith('.jpeg') ||
+            picRes[i].data.url.endsWith('.png') ||
+            picRes[i].data.url.endsWith('.jpg')
+          ) {
+            setTimeout(function () {
+              document.querySelector(
+                '#slideshow'
+              ).innerHTML = `<img src="${picRes[i].data.url}" />`
+            }, 3000)
+            console.log(picRes[i].data.url)
+          }
+        }
       })
       // .catch -> catch errors
       .catch((err) => {
@@ -39,7 +57,8 @@ document.addEventListener('DOMContentLoaded', () => {
         return err
       })
   })
-
+  console.log(document.querySelector('#slideshow'))
+  // document.querySelector('#slideshow').innerHTML = '<img src'
   // append +nsfw:no to that string
   // grab dom elements
   // form id #form
