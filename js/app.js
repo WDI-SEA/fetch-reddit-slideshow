3f7fd0d2a0644c21b062c5652c6ec1cf04738d8d
Consecratio <jcasique@gmail.com>
2021-05-24 20:46:14 -0700

    able to properly query reddit, get image src, and loop through each src, needs styling

--- js/app.js
+++ js/app.js
@@ -4,10 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     let inputForm = document.querySelector("form")
     let imageDisplay = document.querySelector("#displayImages")
     let imageResults = []
+    let iterator = 0
+    let intervalTimer
 
-    // variables to check if src data is an image
-    const imageTypes = [".jpg", ".jpeg", ".png"]
+    // elements to hide/show
+    let pageTitle = document.querySelector("#title")
+    let directionsPTag = document.querySelector("#directions")
+    let stopButton = document.querySelector("#stop")
 
+    // setup event listener to stop images from being shown
+    stopButton.addEventListener("click", showElements)
 
     // REQUEST DATA
     // take form element and prevent default behaviour
@@ -19,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(`User Input: ${userInput}`)
 
         // make fetch request to const api url with given user number
-        fetch(requestUrl + userInput + "&limit=100")
+        fetch(requestUrl + userInput + "+nsfw:no&limit=100")
             // .then --> take response data and format
             .then(res => {
                 console.log("Response came back!")
@@ -28,11 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
             // .then --> use response JSON data
             .then(jsonData => {
                 imageResults = jsonData.data.children
-                                .filter(element => String(element.data.url_overridden_by_dest).includes(".jpg"))
-                                .map(filteredData => filteredData.data.url_overridden_by_dest)
+                                .filter(element => String(element.data.url).includes(".jpg"))
+                                .map(filteredData => filteredData.data.url)
 
                 console.log(imageResults)
-                displayImage(imageResults)
+                hideElements()
+                imageDisplay.src = imageResults[iterator]
+                intervalTimer = setInterval(displayImage, 4000)
             })
             // .catch --> catch errors
             .catch(err => {
@@ -41,19 +49,45 @@ document.addEventListener("DOMContentLoaded", () => {
             })
     })
 
-    function displayImage(results) {
-        imageDisplay.src = results[0]
-    }
     // RESPONSE DATA
-    // collect formated data
-    // function domPeopleList(resArr) {
-    //     resArr.forEach(person => {
-    //         // create an li element for each response
-    //         let li = document.createElement("li")
-    //         li.textContent = person.name.first + " " + person.name.last
-            
-    //         // add li element to DOM
-    //         peopleList.appendChild(li)
-    //     })
-    // }
+    // change image src to an img url stored in imageResults
+    function displayImage() {
+        // display image and reset iterator at end of array
+        iterator++
+        if(iterator == imageResults.length) {
+            iterator = 0
+        }
+
+        imageDisplay.src = imageResults[iterator]
+    }
+
+    function hideElements() {
+        // hide title, directions, and form
+        pageTitle.style.display = "none"
+        directionsPTag.style.display = "none"
+        inputForm.style.display = "none"
+
+        // show stop button and image
+        stopButton.style.display = "block"
+        imageDisplay.style.display = "block"
+    }
+
+    function showElements() {
+        // clear interval
+        clearInterval(intervalTimer)
+
+        // clear results, image, and reset iterator
+        imageResults = []
+        imageDisplay.src = ""
+        iterator = 0
+
+        // show title, directions, and form
+        pageTitle.style.display = "block"
+        directionsPTag.style.display = "block"
+        inputForm.style.display = "block"
+
+        // hide stop button and image
+        stopButton.style.display = "none"
+        imageDisplay.style.display = "none"
+    }
 })
