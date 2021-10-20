c2283bf8a4c06c20db1017ca528185ec8c7addde
cmiltiadou <cmiltiad@gmail.com>
2021-10-20 18:46:44 -0400

    added basic functionality to search reddit and capture correct image url

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 const requestUrl = "https://www.reddit.com/search.json?q="
-console.log('Script is running');
+// console.log('Script is running');
 
+
+// test with forumla for capturing the URL, ammeding with search term, and returning captures amended URL
 document.addEventListener('DOMContentLoaded', () => {
     
     form.addEventListener('submit', (e) =>{
         e.preventDefault()
+        const search = requestUrl+input.value.replaceAll(" " ,  "+") + "+nsfw:no"
         
-        fetch(requestUrl+ input.value) 
+        fetch (search)
           .then((responseData)=>{
-            console.log(requestUrl)
             return responseData.json()
           })
           .then((jsonData)=>{
             console.log("Here is the data:", jsonData);
+            console.log(jsonData.data.children[2].data.url)
+            console.log(search)
           })
+          .catch((error) => {
+            //if any error is sent back, you will have access to it here.
+            console.log('ERROR!!!!')
+            console.log(error)
+        })
         
         console.log('Just fired AJAX request!');
     })
 })
+
+
+let blank = () => {
+ input.value = " "
+}
+
+input.addEventListener("click", blank)
+
