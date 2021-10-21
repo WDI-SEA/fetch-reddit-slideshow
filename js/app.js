d8c4707ab598fc1157216050db9d4a72ccf815fb
cmiltiadou <cmiltiad@gmail.com>
2021-10-20 20:21:38 -0400

    updates app.js to push captured url to img src and display image on page

--- js/app.js
+++ js/app.js
@@ -1,37 +1,50 @@
+//declare startingURL
 const requestUrl = "https://www.reddit.com/search.json?q="
 // console.log('Script is running');
 
 
-// test with forumla for capturing the URL, ammeding with search term, and returning captures amended URL
+// test with function for capturing the URL, ammeding with search term, and returning captures amended URL
 document.addEventListener('DOMContentLoaded', () => {
     
     form.addEventListener('submit', (e) =>{
         e.preventDefault()
+        //ammend searchterm so url does not include space or nsfw images
         const search = requestUrl+input.value.replaceAll(" " ,  "+") + "+nsfw:no"
         
         fetch (search)
-          .then((responseData)=>{
+        .then((responseData)=>{
             return responseData.json()
-          })
-          .then((jsonData)=>{
-            console.log("Here is the data:", jsonData);
-            console.log(jsonData.data.children[2].data.url)
-            console.log(search)
-          })
-          .catch((error) => {
+        })
+        .then((jsonData)=>{
+            // console.log("Here is the data:", jsonData);
+            console.log(jsonData)
+            let imgSrc = jsonData.data.children[5].data.url
+            const addImage = () => {
+                // const img1 = document.createElement("img")
+                image1.src = imgSrc
+            
+            
+            }
+            addImage()
+            
+            // console.log(search)
+
+        })
+        .catch((error) => {
             //if any error is sent back, you will have access to it here.
             console.log('ERROR!!!!')
             console.log(error)
         })
         
-        console.log('Just fired AJAX request!');
+        // console.log('Just fired AJAX request!');
     })
 })
 
 
+
 let blank = () => {
- input.value = " "
+    input.value = " "
 }
+//function to test appending new image to div
 
 input.addEventListener("click", blank)
-
