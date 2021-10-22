21de8ad5e1af45880d0bd6cb236146cdfdd63783
devinrbopp <devinrbopp@gmail.com>
2021-10-21 20:33:29 -0600

    project complete

--- js/app.js
+++ js/app.js
@@ -1,52 +1,80 @@
-// request URL for reddit API
 const requestUrl = 'https://www.reddit.com/search.json?raw_json=1&q='
 
-// has the dom loaded?
 document.addEventListener('DOMContentLoaded', () => {
     console.log('dom loaded hello!')
-    $('#myCarousel').carousel({
-        interval: 5000,
-        cycle: true
-    });
+    $("form").keypress(function(e){
+        if(e.keyCode == 13) {
+            e.preventDefault();
+            return false;
+        }
+    })
     // submit listener
     form.addEventListener('submit', e => {
-        // prevent submit from refreshing the screen
-        e.preventDefault()
-        //create an array to hold the photos
-        // fetch the data
-        fetch(requestUrl + input.value)
-        .then( (responseData) => {
-            return responseData.json()
-        })
-        .then( (jsonData) => {
-            console.log('JSON DATA:')
-            console.log(jsonData.data.children)
-            jsonData.data.children.forEach(addPhoto)            
-        })
-        .catch( (error) => {
-            console.log('ERROR:')
-            console.log(error)
-        })
+        // only proceed if the input has a value
+        if (input.value != '') {
+            // prevent submit from refreshing the screen
+            e.preventDefault()
+            // fetch the data
+            fetch(requestUrl + input.value)
+            .then( (responseData) => {
+                return responseData.json()
+            })
+            .then( (jsonData) => {
+                console.log('JSON DATA:')
+                console.log(jsonData.data.children)
+                // add a reset button to the page
+                addReset()          
+                // add photos to the carousel
+                jsonData.data.children.forEach(addPhoto)  
+            })
+            .catch( (error) => {
+                console.log('ERROR:')
+                console.log(error)
+            })
+        }
     })
     
-    let i = 0
+    let first = true
 
     const addPhoto = redditObj => {
         if (redditObj.data.preview) {
             console.log(redditObj.data.preview.images[0].source.url)
             const slide = document.createElement('div')
             slide.setAttribute('class', 'carousel-item')
-            if (i === 0) {
+            // this 'if' gives first photo the class "active" to trigger the start
+            if (first) {
                 slide.setAttribute('class', 'carousel-item active')
-                i++
+                first = false
             }
-            // WE ARE HAVING AN ISSUE: IF IMAGE DOESN'T EXIST PROCESS STOPS
-            slide.innerHTML = `<img class="d-block w-100" src="${redditObj.data.preview.images[0].source.url}" alt="a photo">`
-            //console.log(slide)
+            // adds img html element to the slide div
+            slide.innerHTML = `<img class="d-block" src="${redditObj.data.preview.images[0].source.url}" alt="a photo">`
             const carouselBox = document.getElementById('carouselBox')
             carouselBox.appendChild(slide)
-            //photoArray.push(redditObj.data.thumbnail)
         }
+    }
+
+    const addReset = () => {
+        const reset = document.createElement('button')
+        reset.setAttribute('id','reset')
+        reset.setAttribute('class','btn btn-primary btn-lg btn-block')
+        reset.setAttribute('type','button')
+        input.setAttribute('class','d-none')
+        instructions.setAttribute('class','d-none')
+        reset.innerText = 'Reset'
+        reset.addEventListener('click', resetPage)
+        submit.setAttribute('class', 'd-none')
+        form.appendChild(reset)
+    }
+    
+    const resetPage = () => {
+        console.log('reset clicked')
+        submit.setAttribute('class', 'btn btn-primary btn-lg')
+        input.setAttribute('class','form-control mt-2 mb-2')
+        instructions.setAttribute('class','')
+        form.removeChild(reset)
+        carouselBox.innerHTML = ''
+        input.value = ''
+        first = true
     }
 })
 
