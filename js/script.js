baaafd97969dce86d129f87194a1a149873fd302
hannahlegros <hannahrlegros@gmail.com>
2021-11-03 23:23:49 -0400

    js and html functioning

--- js/script.js
+++ js/script.js
@@ -1,35 +1,68 @@
-const requestURL = "https://www.reddit.com/search.json?q="
-
+let resultIndex = 0
+let form = document.getElementById("form")
+let stopButton = document.querySelector("button")
+let headerOne = document.querySelector("h1")
+let headerTwo = document.querySelector("h2")
 document.addEventListener("DOMContentLoaded", () => {
-    
+    //change visibility of images
+    slideshow.style.display = "none"
+    // add event listener for submit button in form
     form.addEventListener("submit", (e)=>{
         e.preventDefault()
-        fetch(requestURL+input.value)
+        const searchTerm = userInput.value.replaceAll(" ", "+")
+        fetch(`https://www.reddit.com/search.json?q=${searchTerm}&raw_json=1`)
         .then((responseData)=>{
             return responseData.json()
         })
+        // function to select photos from reddit
         .then((jsonData)=>{
-            let photoArray = []
-            // function to select photos from reddit
-            const getPhoto = (objSearched) => {
-                let photo = document.createElement('img')
-                photo.src = objSearched.data.thumbnail
-                thumbnailPhotos = objSearched.data.thumbnail
-                allPictures.appendChild(photo)
-                photoArray.push(thumbnailPhotos)
-            }                
-            // console.log(photoArray)
-            jsonData.data.children.forEach(getPhoto)
-            let i = 0
-            const startSlideshow = () => {
-                for (i = 0; i < photoArray.length; i++){
-                    console.log("this is my current photo:\n", photoArray[i])
-                    photoArray[i]
+            // make form and headings disappear while photos are cycling
+            headerOne.style.visibility = "hidden"
+            headerTwo.style.visibility = "hidden"
+            form.style.visibility = "hidden"
+            let resultsArray = jsonData.data.children
+            let photoArray = resultsArray.map((resultObj)=>{
+                if(resultObj.data.preview){
+                    return resultObj.data.preview.images[0].source.url
+                } else {
+                    return "No image"
+                }
+            })
+            // filter results to only ones with images
+            const filteredURLs = photoArray.filter((url)=>{
+                if(url==="No image"){
+                    return false
+                }
+                else {
+                    return true
+                }
+            })
+            // loop through photos
+            const changeSrc = () => {
+                if(resultIndex<filteredURLs.length){
+                    slideshow.setAttribute("src", filteredURLs[resultIndex])
+                    resultIndex++
+                } 
+                else{
+                    resultIndex = 0
                 }
             }
-            // set Interval for photos 
-            setInterval(startSlideshow, 2000)
-        })
+            changeSrc()
+            slideshow.style.display = "block"
+            // set Interval for photos
+            const changeSlide = setInterval(changeSrc, 3000)
+            // make stop button that reverts back to landing page (clears)
+            // add event listener for stop button
+            stopButton.addEventListener("click", ()=>{
+                clearInterval(changeSlide)
+                document.getElementById("userInput").innerText = ""
+                slideshow.style.display = "none"
+                headerOne.style.visibility = "visible"
+                headerTwo.style.visibility = "visible"
+                form.style.visibility = "visible"
+                resultIndex = 0
+            })
+        })    
         .catch((error)=>{
             console.log("ERROR:\n", error)
         })
@@ -37,8 +70,34 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-// loop through photos
-// make form and headings disappear while photos are cycling
-// make stop button that reverts back to landing page (clears)
 
-//change visibility of divs/images
+
+
+
+
+
+//     }
+//     
+//     const getPhoto = (objSearched) => {
+//         let photo = document.createElement('img')
+//         photo.src = objSearched.data.thumbnail
+//         thumbnailPhotos = objSearched.data.thumbnail
+//         allPictures.appendChild(photo)
+//         photoArray.push(thumbnailPhotos)
+//     }                
+//     // console.log(photoArray)
+//     let i = 0
+//     const startSlideshow = () => {
+//         for (i = 0; i < photoArray.length; i++){
+//             console.log("this is my current photo:\n", photoArray[i])
+//             photoArray[i]
+//         }
+//     }
+//      
+//     setInterval(startSlideshow, 2000)
+// })
+// for(let i=0; i<photoArray.length; i++){
+            //     let redditPhoto = document.createElement("img")
+            //     redditPhoto.setAttribute("src", photoArray[i])
+            //     allPictures.appendChild()
+            // }
