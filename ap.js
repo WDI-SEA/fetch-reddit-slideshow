44b8270164502b388b2e6dbd46a38f76d58aaabd
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-12-27 00:40:35 -0500

    fetch data working

--- ap.js
+++ ap.js
@@ -1,16 +1,21 @@
 const url = 'https://www.reddit.com/search.json?q='
 document.addEventListener('DOMContentLoaded', () => {
+
+
     form.addEventListener('submit', (e) => {
         e.preventDefault()
-     
-        console.log('hello', e);
-
-        fetch(url+input.value)
+        const inputValue = userInput.value
+    //    console.log(inputValue)
+        fetch(url+inputValue)
         .then(resData => {
-           resData.json()
+           return resData.json()
         })
         .then(jsonData => {
-            console.log(jsonData);
+            console.log(jsonData)
+        })
+        .catch(error=> {
+            console.log(error)
+            
         })
     })
 })
