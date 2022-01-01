d4c24b9ba7c5f9c140bce1fc9f20c4d4efaebbc4
Lawrencesalinas <lawrencesalinas@yahoo.com>
2021-12-31 19:41:14 -0500

    added a stop button

--- ap.js
+++ ap.js
@@ -1,5 +1,5 @@
 const url = 'https://www.reddit.com/search.json?q='
-const time = 2000
+const time = 1500
 let slideshowInterval = null
 let i = 0
 document.addEventListener('DOMContentLoaded', () => {
@@ -30,50 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
                     return true
                 }
             })
-            // console.log(imgUrls);
-            // const filteredURLs =imgUrls.filter(url => {
-            //     if(url.includes('jpg') || url.includes('.png')){
-            //         return true
-            //     } else {
-            //         return false
-            //     }
-            // })
-
-
-            // for(let i=0; i<imgUrls.length; i++){
-            //     //create a new image element
-            //     let redditPhoto = document.createElement('img')
-            //     //add the src from the array
-            //     redditPhoto.setAttribute('src', imgUrls[i])
-            //     //append
-            //     testImageList.appendChild(redditPhoto)
-            // }
-            // console.log(filteredURLs);
-           
-
-            // const changeScreen = () => {
-            //     if(resultIndex < filteredURLs.length - 1){ 
-            //         slideshow.setAttribute('src', filteredURLs([resultIndex]))
-            //         resultIndex++
-            //     } else {
-            //         resultIndex = 0
-            //     }
-            // }
             const changeImg = () => {
-              
-                
-                if(i < filteredURLs.length -1){
-                    slide.setAttribute('src', filteredURLs[i])
-                        i++
-                    } else {
-                        i = 0;
-                    }
+              if(i < filteredURLs.length -1){
+                slide.setAttribute('src', filteredURLs[i])
+                i++
+              } else {
+                    i = 0;
+              }
             }
-            setInterval(changeImg, time)
+            const slideShowInterval = setInterval(changeImg, time)
+            const stopSlideshow = () => {clearInterval(slideShowInterval)}
+            stopButton.addEventListener('click', () => {
+                stopSlideshow()
+            })
         })
+
         .catch(error=> {
             console.log(error)
-
         })
     })
 })
