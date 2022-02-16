4de2d0e30c3938d4d5639e608201bcfb45b7f502
snacksident <leak187@gmail.com>
2022-02-15 21:23:12 -0800

    add functionality to produce images on screen all at once

--- js/script.js
+++ js/script.js
@@ -2,39 +2,78 @@ const submitForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-input')
 const mainContainer = document.querySelector("#main-container")
 const slideshowContainer = document.querySelector("#slideshow-container")
+const slideshowDisplay = document.querySelector("#slideshow-display")
+
+let searchedObjects = []
+let imageObjects = []
 
 submitForm.addEventListener('submit',(e)=>{
     e.preventDefault()
+    resetDisplay()
     fetchRedditData(searchInput.value)
-    // divToggler(mainContainer)
-    // divToggler(slideshowContainer)
 })
 
+//fetch from reddit api based on given search string
 const fetchRedditData = (searchString) =>{
+    // const endpoint = `http://www.reddit.com/search.json?q=${searchString}+nsfw:no`
     const endpoint = `http://www.reddit.com/search.json?q=${searchString}+nsfw:no`
+    //might need raw_json=1 as an arg in the endpoint to help with image handling?
     // fetch requires the api url as arg
     // fetch(endpoint) // returns fetch object
     fetch(endpoint)
-    .then(fetchObj=>fetchObj.json()) // implicitly returning the result of fetchObj.json()
+    .then(fetchObj=>fetchObj.json())
     .then(jsonData=>{
-        // console.log(jsonData.data.children)
-
-        //displays all URLS - not all URLS are in image.  might be question for TA
         jsonData.data.children.forEach(element=>{
-            console.log(element.data.url)
+            //store all objects into an array
+            searchedObjects.push(element)
         })
-
-
+        findImageObjects()
+        displayImages()
         
     })
     .catch(err=>console.log('error fetching data',err))
+
+
+
 }
 
-function divToggler() {
-    let x = document.querySelector("#main-container")
+function findImageObjects(){
+    //filter out all objects that directly link to an image - array.filter()
+    // imageObjects = searchedObjects.filter(element=>{
+    //     element.data.url.endsWith(".jpg" || ".png" || ".gif")
+    // })
+    for(let i = 0; i < searchedObjects.length; i++){
+        if(searchedObjects[i].data.url.includes(".jpg" || ".png")){
+            imageObjects.push(searchedObjects[i])
+        }
+    }
+}
+
+function displayImages(){
+    //iterate over all objects that had ONE single image
+    
+    imageObjects.forEach(image => {
+        console.log(image.data.url)
+        //create a new img tag
+        let newImg = document.createElement('img')
+        //give img new source
+        newImg.src = image.data.url
+        //append image to slideshowDisplay
+        slideshowDisplay.appendChild(newImg)
+    })
+}
+
+function containerToggler(x) {
     if (x.style.display === "none") {
       x.style.display = "block"
     } else {
       x.style.display = "none"
     }
-  }
+}
+function resetDisplay(){
+    while(slideshowDisplay.firstChild){
+        slideshowDisplay.removeChild(slideshowDisplay.firstChild)
+    }
+    searchedObjects = []
+    imageObjects = []
+}
