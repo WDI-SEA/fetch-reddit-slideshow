96f20bcd48a3e12905c548b6af0cd3d9660830da
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-15 23:49:01 -0800

    add style

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
 const fetchit = qStr => {
-  const endpoint = `http://www.reddit.com/search.json?q=${qStr}&limit=10000`
-  console.log(endpoint)
+  // reddit .json api maximum limit set at 100
+  const endpoint = `http://www.reddit.com/search.json?q=${qStr}&limit=100`
   fetch(endpoint)
     .then(fetchObj => fetchObj.json())
     .then(jsonData => {
       console.log(jsonData)
       const arrListing = jsonData.data.children
       console.log(arrListing.length)
-      let childImg = document.querySelector('#dataHolder')
-      while(childImg.firstChild) {
-          childImg.firstChild.remove()
+      let childImg = document.querySelector('#image-holder')
+      while (childImg.firstChild) {
+        childImg.firstChild.remove()
       }
       for (let i = 0; i < arrListing.length; i++) {
+
+        let newImgContainer = document.createElement('div')
+        newImgContainer.width = 'fit-content'
+        newImgContainer.style.border = '.1rem solid grey'
+        newImgContainer.id = `id${i}`
+        document.querySelector('#image-holder').appendChild(newImgContainer)
+
         let listData = document.createElement('img')
         listData.src = jsonData.data.children[i].data.url
-        listData.style.width= '300px'
-        document.querySelector('#dataHolder').appendChild(listData)
+        listData.style.width = '400px'        
+        document.querySelector(`#id${i}`).appendChild(listData)
         // if img src is invalid, hide the image.
-        listData.addEventListener('error', errHandler => listData.style.display='none') 
-        
+        listData.addEventListener(
+          'error',
+          errHandler => (newImgContainer.style.display = 'none')
+        )
       }
     })
     .catch(err => console.log(err))
@@ -32,6 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.target.nodeName === 'BUTTON') {
       queryString = document.getElementById('queryString').value
       fetchit(queryString)
+      document.getElementById('input-box').style.display ='none'
     }
   })
 })
