99d7ebc07faf65f988b9d75b53a3d0c2dc984528
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-16 22:49:06 -0800

    fix slideshow and check url

--- js/script.js
+++ js/script.js
@@ -1,47 +1,63 @@
-const fetchit = qStr => {
+function fetchit (qStr) {
+  const imgList = []
   // reddit .json api maximum limit set at 100
   const endpoint = `http://www.reddit.com/search.json?q=${qStr}&limit=100`
   fetch(endpoint)
     .then(fetchObj => fetchObj.json())
     .then(jsonData => {
-      console.log(jsonData)
       const arrListing = jsonData.data.children
-      console.log(arrListing.length)
-      let childImg = document.querySelector('#image-holder')
-      while (childImg.firstChild) {
-        childImg.firstChild.remove()
-      }
       for (let i = 0; i < arrListing.length; i++) {
-
-        let newImgContainer = document.createElement('div')
-        newImgContainer.width = 'fit-content'
-        newImgContainer.style.border = '.1rem solid grey'
-        newImgContainer.id = `id${i}`
-        document.querySelector('#image-holder').appendChild(newImgContainer)
-
-        let listData = document.createElement('img')
-        listData.src = jsonData.data.children[i].data.url
-        listData.style.width = '400px'        
-        document.querySelector(`#id${i}`).appendChild(listData)
-        // if img src is invalid, hide the image.
-        listData.addEventListener(
-          'error',
-          errHandler => (newImgContainer.style.display = 'none')
-        )
+        if (checkURL(jsonData.data.children[i].data.url)) {
+          setTimeout(function () {
+            imageit(jsonData.data.children[i].data.url)
+            document.getElementById('reset').style.display = 'inline-block'
+          }, 2000 * i)
+        }
       }
     })
     .catch(err => console.log(err))
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('hello')
-  let queryString
-  document.addEventListener('click', e => {
-    e.preventDefault()
-    if (e.target.nodeName === 'BUTTON') {
-      queryString = document.getElementById('queryString').value
+function checkURL (url) {
+  if (typeof url !== 'string') return false
+  return url.match(/\.(jpg|jpeg|gif|png)$/) != null
+}
+
+function imageit (imgURL) {
+  let parent = document.querySelector('#image-holder')
+  while (parent.firstChild) {
+    parent.firstChild.remove()
+  }
+  let listData = document.createElement('img')
+  listData.src = imgURL
+  document.querySelector('#image-holder').appendChild(listData)
+  // if img src is invalid, hide the image.
+  listData.addEventListener(
+    'error',
+    errHandler => (listData.style.display = 'none')
+  )
+}
+function reinitialise () {
+  location.reload()
+}
+
+let queryString
+let pageMessage = ''
+document.addEventListener('click', e => {
+  e.preventDefault()
+
+  if (e.target.id === 'search') {
+    queryString = document.getElementById('queryString').value
+    if (queryString.length == 0) {
+      document.querySelector('#image-holder').innerText =
+        'Empty search not accepted.'
+    } else {
+      document.querySelector('#image-holder').innerText = 'Loading slideshow...'
+      document.getElementById('input-box').style.display = 'none'
       fetchit(queryString)
-      document.getElementById('input-box').style.display ='none'
     }
-  })
+  } else if (e.target.id === 'reset') {
+    document.getElementById('input-box').style.display = 'initial'
+    reinitialise()
+  }
 })
