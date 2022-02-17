bb0bc963df1b4957708d78a635c7fea8da518af3
Norman Teodoro <normanteodoro@hotmail.com>
2022-02-17 12:06:53 -0800

    code cleanup

--- js/script.js
+++ js/script.js
@@ -5,61 +5,19 @@ function fetchit (qStr) {
   fetch(endpoint)
     .then(fetchObj => fetchObj.json())
     .then(jsonData => {
-      const arrListing = jsonData.data.children
-      for (let i = 0; i < arrListing.length; i++) {
-        if (checkURL(jsonData.data.children[i].data.url)) {
-          setTimeout(function () {
-            imageit(jsonData.data.children[i].data.url)
-            document.getElementById('reset').style.display = 'inline-block'
-          }, 2000 * i)
-        }
-      }
-    })
-    .catch(err => console.log(err))
-}
-
-function checkURL (url) {
-  if (typeof url !== 'string') return false
-  return url.match(/\.(jpg|jpeg|gif|png)$/) != null
-}
-
-function imageit (imgURL) {
-  let parent = document.querySelector('#image-holder')
-  while (parent.firstChild) {
-    parent.firstChild.remove()
-  }
-  let listData = document.createElement('img')
-  listData.src = imgURL
-  document.querySelector('#image-holder').appendChild(listData)
-  // if img src is invalid, hide the image.
-  listData.addEventListener(
-    'error',
-    errHandler => (listData.style.display = 'none')
-  )
-}
-function reinitialise () {
-  location.reload()
-}
-function fetchit (qStr) {
-  const imgList = []
-  // reddit .json api maximum limit set at 100
-  const endpoint = `http://www.reddit.com/search.json?q=${qStr}&limit=100`
-  fetch(endpoint)
-    .then(fetchObj => fetchObj.json())
-    .then(jsonData => {
-      const arrListing = jsonData.data.children.filter(function (imgURL) {
-        return imgURL.data.url
-      })
-      const arrImgSrc = arrListing.filter(function (imgURL) {
-        if (checkURL(imgURL.data.url)) {
+      const arrListing = jsonData.data.children.filter(function(imgURL){
           return imgURL.data.url
-        }
       })
-      for (let i = 0; i < arrImgSrc.length; i++) {
-        setTimeout(() => {
-          imageit(arrImgSrc[i].data.url)
-          document.getElementById('reset').style.display = 'inline-block'
-        }, 2000 * i)
+      const arrImgSrc = arrListing.filter(function(imgURL){
+          if (checkURL(imgURL.data.url)) {
+              return imgURL.data.url
+          }
+      })
+      for (let i=0;i < arrImgSrc.length;i++) {
+          setTimeout(() => {
+              imageit(arrImgSrc[i].data.url)
+              document.getElementById('reset').style.display = 'inline-block'                
+          }, 2000 * i);
       }
     })
     .catch(err => console.log(err))
@@ -87,27 +45,6 @@ function imageit (imgURL) {
 function reinitialise () {
   location.reload()
 }
-
-let queryString
-let pageMessage = ''
-document.addEventListener('click', e => {
-  e.preventDefault()
-
-  if (e.target.id === 'search') {
-    queryString = document.getElementById('queryString').value
-    if (queryString.length == 0) {
-      document.querySelector('#image-holder').innerText =
-        'Empty search not accepted.'
-    } else {
-      document.querySelector('#image-holder').innerText = 'Loading slideshow...'
-      document.getElementById('input-box').style.display = 'none'
-      fetchit(queryString)
-    }
-  } else if (e.target.id === 'reset') {
-    document.getElementById('input-box').style.display = 'initial'
-    reinitialise()
-  }
-})
 
 let queryString
 let pageMessage = ''
