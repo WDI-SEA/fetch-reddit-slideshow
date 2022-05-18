46a76d0ec32186d085424cfe709c730c8a2a81f1
Hoan-K-Le <hoanleecr@gmail.com>
2022-05-18 09:03:09 -0700

    deliverable

---
+++ js/app.js
@@ -0,0 +1,43 @@
+console.log('hi')
+
+const url = document.querySelector('#user-input')
+const options = {
+    headers: {
+        Accept: 'application/json'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    
+    document.querySelector('#form').addEventListener('submit', e => {
+        e.preventDefault()
+        // fetch image from reddit
+        const url = document.querySelector('#user-input')
+        const redditUrl = `https://randomuser.me/api/?results=?${url.value}`
+        fetch(redditUrl, options)
+        // unjsonify the data
+        .then(response => response.json())
+        .then (jsonData => {
+            let imgArray = jsonData.data.children
+            imgArray = imgArray((i) => {
+                return i.data.post_hint === "image"
+            })
+            // const image = new Image ()
+            // image.src = imgArray[0]
+        })
+        // clear out the container
+        
+        //create a tag for the img
+        //set the img to be the front page
+        
+        
+        
+    // })
+})
+    
+    // .catch(err => {
+    //     console.warn('uh oh', err)
+    // })
+})
+    
