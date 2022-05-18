bfa8cf2ce97f2ba7f74133c55169c08c4716cc09
“erhaneth” <gumusucb21@gmail.com>
2022-05-18 00:01:55 -0700

    Can not reach 'cat' images

--- js/app.js
+++ js/app.js
@@ -1,33 +1,45 @@
 // console.log('I am here')
 //Global variables
-const redditApi = 'https://www.reddit.com/search.json?nsfw=no&q='
+const redditApi = 'http://www.reddit.com/search.json?q=cats+nsfw:no'
 let currentImg = []
 
 
 //Event Listener
-document.getElementById('go').addEventListener('click', e => {
-    //Prevent default form submission
-    e.preventDefault()
-    let userInput = document.getElementById('go-input').value
-    //Search from reddit
-    if (userInput) {
-        //Empty Form
-        document.getElementById('go-input').value = ''
-    } else {
-        console.log('here')
-    }
+// document.getElementById('go').addEventListener('click', e => {
+//     //Prevent default form submission
+//     e.preventDefault()
+//     let userInput = document.getElementById('go-input').value
+//     //Search from reddit
+//     if (userInput) {
+//         //Empty Form
+//         document.getElementById('go-input').value = ''
+//     } else {
+//         console.log('here')
+//     }
 
-    fetch(redditApi)
-        .then(response => response.json())
-        .then(data => console.log(data.children));
+//     fetch(redditApi)
+//         .then(response => response.json())
+//         .then(data => console.log(data.children));
         
-    //  console.log(redditApi)
-})
+//      console.log(redditApi)
+// })
+fetch(redditApi)
+// console.log(fetch(redditApi))
+        .then(function(response){
+            // console.log(response.json())
+            return response.json()
+        })
+        .then(function(response){
+            console.log(response)
+            // return response.json()
+        })
 
+        
 
 
 //Functions 
-
+currentImg = data.children[1]
+console.log(data.children[1])
 
 //Use AJAX to make a request
 
