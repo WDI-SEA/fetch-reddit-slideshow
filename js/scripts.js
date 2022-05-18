f96bd91d38341d7429237b527fa5382f3cb2e84f
Emily-Herndon <emilyvherndon@gmail.com>
2022-05-18 09:16:17 -0700

    added comments to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -5,42 +5,59 @@ const submitButton = document.querySelector('#submit-button')
 const image = document.querySelector('img')
 const clearButton = document.querySelector('#clear-button')
 const section = document.querySelector('#section')
-let imgUrlArr = []
+let imgUrlArray = []
 let l = 0
 let startSlideShow 
 
 document.addEventListener('DOMContentLoaded', () => {
     submitForm.addEventListener('submit', (e) => {
         e.preventDefault()
+        // url with user input search term/phrase
         const redditSearch = `http://www.reddit.com/search.json?q=${userInput.value}+nsfw:no&limit=150`
         fetch(redditSearch)
+            //turn it to json
             .then(responseData => responseData.json())
             .then(jsonData => {
-                imgUrlArr = jsonData.data.children
-                imgUrlArr = imgUrlArr.filter((i)=> {
+                //get the inside info
+                imgUrlArray = jsonData.data.children
+                //only pull photos
+                imgUrlArray = imgUrlArray.filter((i)=> {
                     return i.data.post_hint === "image"
                 })
-                imgUrlArr= imgUrlArr.map((j)=>{
+                //create new array
+                imgUrlArray= imgUrlArray.map((j)=>{
+                    //pop. array w/url of images
                     return j.data.url
                 })
                 // console.log(imgUrlArr)
             })
+            //hide the form/title/instructions
             section.classList.add('hidden')
+            //reveal image container
             imageContainer.classList.remove('hidden')
-            startSlideShow = setInterval(() => {
-                image.src = imgUrlArr[l] 
+            //set interval for every 2 seconds
+            slideShow = setInterval(() => {
+                //change source of img to diff index of image url array
+                image.src = imgUrlArray[l] 
+                //increase index number on every itteration
                 l++
-                if(l >= imgUrlArr.length){
+                //once reach the end of the array start back at the beginning
+                if(l >= imgUrlArray.length){
                     l = 0
                 }
             }, 2000)    
 
     })
     clearButton.addEventListener('click', () => {
-        clearInterval(startSlideShow)
+        //clear the slidshow interval
+        clearInterval(slideShow)
+        //show title/form/instructions again
         section.classList.remove('hidden')
+        //reset image source to nothing
         image.src = ""
+        //empties form for user to search something new
         userInput.value = ""
+        //hide image container
         imageContainer.classList.add('hidden')
     })
 })
