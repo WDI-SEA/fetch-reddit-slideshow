37d9458e36f8a33061482110ad31d525c7f50959
“erhaneth” <gumusucb21@gmail.com>
2022-05-17 23:22:16 -0700

    Add fetch function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
 // console.log('I am here')
 //Global variables
 const redditApi = 'https://www.reddit.com/search.json?nsfw=no&q='
+let currentImg = []
 
 
 //Event Listener
 document.getElementById('go').addEventListener('click', e => {
     //Prevent default form submission
- e.preventDefault()
- let userInput = document.getElementById('go-input').value
- //Search from reddit
- if(userInput) {
-     //Empty Form
-     document.getElementById('go-input').value = ''
- } else {
-     console.log('here')
- }
+    e.preventDefault()
+    let userInput = document.getElementById('go-input').value
+    //Search from reddit
+    if (userInput) {
+        //Empty Form
+        document.getElementById('go-input').value = ''
+    } else {
+        console.log('here')
+    }
+
+    fetch(redditApi)
+        .then(response => response.json())
+        .then(data => console.log(data.children));
+        
+    //  console.log(redditApi)
 })
 
 
 
-
+//Functions 
 
 
 //Use AJAX to make a request
@@ -30,5 +37,4 @@ document.getElementById('go').addEventListener('click', e => {
 //Hide results
 //Stop Button Click
 //Go Button Click
-//Functions 
 //Call reddit API with ajax and using fetch function
