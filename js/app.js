4225e5cd6670e42c29af1dca985d661491b89293
“erhaneth” <gumusucb21@gmail.com>
2022-05-17 21:34:34 -0700

    Add event listener for 'go' button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('I am here')
+// console.log('I am here')
+//Global variables
+const redditApi = 'https://www.reddit.com/search.json?nsfw=no&q='
+
+
+//Event Listener
+document.getElementById('go').addEventListener('click', e => {
+    //Prevent default form submission
+ e.preventDefault()
+ let userInput = document.getElementById('go-input').value
+ //Search from reddit
+ if(userInput) {
+     //Empty Form
+     document.getElementById('go-input').value = ''
+ } else {
+     console.log('here')
+ }
+})
+
+
+
+
+
+
+//Use AJAX to make a request
+
+//Create button to stop animation
+
+
+//Hide results
+//Stop Button Click
+//Go Button Click
+//Functions 
+//Call reddit API with ajax and using fetch function
