0ec5aa5e476e40e852aa84669ba425494184e1cd
devin-lynch <devin.l.lynch@gmail.com>
2022-08-29 18:56:21 -0500

    Add pseudocode

--- js/script.js
+++ js/script.js
@@ -8,14 +8,20 @@ document.addEventListener('DOMContentLoaded', () => {
     form.addEventListener('submit', e => {
         e.preventDefault()
         console.log(input.value)
+        // step 1 -- fetch data
         fetch(url)
+            //step 2 -- jsonify data
             .then(searchData => {
                 return searchData.json()
             })
-            // .then(console.log)
+            // step 3 -- do something neat with json data
             .then(searchJson => {
-                
+                searchJson.results.forEach(result => {
+                    console.log(result)
+                })
             })
+            // step 4 -- be a good programmer and handle errors
+            .catch(console.warn)
     })    
    
 
