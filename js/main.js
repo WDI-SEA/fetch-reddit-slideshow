2e207f187e783bb5951fec73f5eec305fb3dfd15
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-29 16:30:02 -0700

    add hideable section and set consts

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const form = document.querySelector('form')
+const section = document.querySelector('section')
+const div = document.querySelector('div')
+
+console.log(button, body, form, section, div)
+
+})
