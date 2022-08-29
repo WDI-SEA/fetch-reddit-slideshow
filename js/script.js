29af0d1d9b0979618333ebf7bdb419df92bdd0a9
devin-lynch <devin.l.lynch@gmail.com>
2022-08-29 18:42:08 -0500

    Console log input value

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('Hello!')
+document.addEventListener('DOMContentLoaded', () => {
+
+    const searchButton = document.querySelector('#search-button')
+    const input = document.querySelector('input')
+    const form = document.querySelector('form')
+    const url = `https://www.reddit.com/search.json?q=${input.value}+nsfw:no`
+    
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        console.log(input.value)
+        fetch(url)
+            .then(searchData => {
+                return searchData.json()
+            })
+            // .then(console.log)
+            .then(searchJson => {
+                
+            })
+    })    
+   
+
+})
