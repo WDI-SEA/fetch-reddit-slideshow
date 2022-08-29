3fa3ffa8f0c93c02b25254b95ccf052760e290d7
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-29 16:58:11 -0700

    more styling.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 const form = document.querySelector('form')
+const input = document.querySelector('#searchBar')
+const button = document.querySelector('button')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     // console.log('working')
+    input.value = ''
+    input.style.display = 'none'
+    button.innerText = 'END'
+    button.addEventListener('click', () => {
+        console.log('working')
+        input.style.display = 'flex'
+    })
+
 })
