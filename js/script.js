5f33182abe7b2ae48f22a903b9672f40e3b3c31f
devin-lynch <devin.l.lynch@gmail.com>
2022-08-29 20:57:42 -0500

    Ensure images populate upon search

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.querySelector('#search-button')
     const input = document.querySelector('input')
     const form = document.querySelector('form')
-    const url = `https://www.reddit.com/search.json?q=${input.value}+nsfw:no`
-    
+    const hideDiv = document.querySelector('#hide')
+    const body = document.queryCommandIndeterm('body')
+    const results = document.querySelector('#results')
+
     form.addEventListener('submit', e => {
         e.preventDefault()
+        const url = `https://www.reddit.com/search.json?q=${input.value}+nsfw:no`
         console.log(input.value)
         // step 1 -- fetch data
         fetch(url)
@@ -16,13 +19,21 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             // step 3 -- do something neat with json data
             .then(searchJson => {
-                searchJson.results.forEach(result => {
-                    console.log(result)
+                console.log(searchJson.data.children)
+                const imgParent = searchJson.data.children
+                // console.log(imgParent[0].data.thumbnail)
+                imgParent.forEach(result => {
+                    const img = document.createElement('img')
+                    img.src = result.data.thumbnail
+                    results.appendChild(img)
                 })
-            })
+               
+                })
+    })
+})
             // step 4 -- be a good programmer and handle errors
-            .catch(console.warn)
-    })    
+            // .catch(console.warn)
+      
    
 
-})
+
