6a9934aae0edbf708d9a2b5d093ca72a2301024c
brandon-w1205 <bwu1205@gmail.com>
2022-08-29 21:55:59 -0700

    adds style none and block when showing and removing displays

--- js/script.js
+++ js/script.js
@@ -3,14 +3,45 @@ addEventListener('DOMContentLoaded', () => {
     const typingArea = document.querySelector('#sInput')
     const startButton = document.querySelector('#startSlide')
     const stopButton = document.querySelector('#stopSlide')
+    const heading = document.querySelector('h1')
+
     let buttonValue = ""
     let pictureArray = [];
+    const images = document.createElement('img')
+    const heading2 = document.querySelector('h2')
+    heading2.style.display = 'none'
+    let anInterval;
+    let loadingTimer;
 
+    function hideExtra() {
+        heading.style.display = 'none'
+        typingArea.style.display = 'none'
+        startButton.style.display = 'none'
+        stopButton.style.display = 'block'
+        loadingTimer = setTimeout(() => {
+            heading2.style.display = 'block'
+        }, 0)
+    }
+
+    function showExtra() {
+        heading.style.display = 'block'
+        typingArea.style.display = 'block'
+        startButton.style.display = 'block'
+        stopButton.style.display = 'none'
+    }
+    
     // Search button event listener for click
     startButton.addEventListener('click', (e) => {
         e.preventDefault()
         // sets the the variable "buttonValue" to whatever is in the Search box
         buttonValue = typingArea.value
+
+        hideExtra()
+
+        setTimeout(() => {
+            heading2.style.display = 'none'
+            images.style.display = 'block'
+        }, 2050)
 
         // sets url to search up anything on reddit that is under 18 friendly content
         const url = `https://www.reddit.com/search.json?q=${buttonValue}&over_18=false`
@@ -20,15 +51,19 @@ addEventListener('DOMContentLoaded', () => {
                 return theData.json()
             })
             .then(dataJson => {
+                pictureArray = [];
                 for(let i = 0; i < dataJson.data.children.length; i++) {
-                    pictureArray.push(dataJson.data.children[i].data.url)
+                    if(dataJson.data.children[i].data.domain === "i.redd.it"){
+                        pictureArray.push(dataJson.data.children[i].data.url)
+                    }
+                    
                 }
                 return pictureArray
             })
             .then(theArray => {
-                const images = document.createElement('img')
+                
                 let k = 0;
-                setInterval(() => {
+                anInterval = setInterval(() => {
                     images.src = `${theArray[k]}`
                     images.alt = `${buttonValue} result`
                     area.append(images)
@@ -42,6 +77,13 @@ addEventListener('DOMContentLoaded', () => {
                 console.warn(err)
             })
     })
-
     
+    stopButton.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        showExtra()
+        
+        clearInterval(anInterval)
+        images.style.display = 'none'
+    })
 })
