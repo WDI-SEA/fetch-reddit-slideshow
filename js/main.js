25948ebd48ead12ac029c9df8a1c4e4f21d7de2d
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-29 19:12:53 -0700

    finish clear button functionality

--- js/main.js
+++ js/main.js
@@ -1,38 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const input = document.querySelector('#search-input')
-const form = document.querySelector('form')
-const section = document.querySelector('section')
-const div = document.querySelector('div')
-form.addEventListener('submit', e =>{
-    section.style.display = "none"
-    e.preventDefault()
-    const redditURL = `http://www.reddit.com/search.json?q=${input.value}+nsfw:no`
-    fetch(redditURL)
-        .then(response => {
-            return response.json()
-            
-        })
-        .then(jsonData =>{
-            console.log(jsonData.data.children)
-            jsonData.data.children.forEach(result => {
-                const img = document.createElement('img')
-                img.src = result.data.thumbnail
-                div.appendChild(img)
-            });
-            const clearButton = document.createElement('button')
-            clearButton.innerText = "clear"
-            body.append(clearButton)
-            clearButton.addEventListener('click', (e) => {
-                while(div.firstChild){
-                    div.remove()
-                    break
-                }
-                section.style.display = flex
+    const button = document.querySelector('button')
+    const body = document.querySelector('body')
+    const input = document.querySelector('#search-input')
+    const form = document.querySelector('form')
+    const section = document.querySelector('section')
+    const div = document.querySelector('div')
+    const slideshowContainer = document.querySelector('#slideshow-container')
+    
+    form.addEventListener('submit', e =>{
+        e.preventDefault()
+        //hide 
+        section.style.display = "none"
+        const redditURL = `http://www.reddit.com/search.json?q=${input.value}+nsfw:no`
+        
+        fetch(redditURL)
+            .then(response => {
+                return response.json()
             })
-        })
-})
+
+            .then(jsonData =>{
+                console.log(jsonData.data.children)
+                jsonData.data.children.forEach(result => {
+                    const img = document.createElement('img')
+                    img.src = result.data.thumbnail
+                    div.appendChild(img)
+                });
+                const clearButton = document.createElement('button')
+                clearButton.style.display = "inline"
+                clearButton.innerText = "clear"
+                body.append(clearButton)
+                clearButton.addEventListener('click', (e) => {
+                    while(div.firstChild){
+                        div.innerHTML = ''
+                        break
+                    }
+                    section.style.display = "flex"
+                    clearButton.style.display = "none"
+                    input.value = ''
+                })
+            })
+    })
+
 
 
 
