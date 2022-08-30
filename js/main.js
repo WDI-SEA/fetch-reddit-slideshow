cd273dd531e3f459464a5f4bc7c2eb68247349ab
waltersmatthew <waltersmatthew88@gmail.com>
2022-08-29 21:47:50 -0700

    done for the night

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.querySelector('#search-input')
     const form = document.querySelector('form')
     const section = document.querySelector('section')
-    const div = document.querySelector('div')
-    const slideshowContainer = document.querySelector('#slideshow-container')
+    const slideDiv = document.querySelector('#slideshow-container')
     
     form.addEventListener('submit', e =>{
         e.preventDefault()
@@ -19,19 +18,32 @@ document.addEventListener('DOMContentLoaded', () => {
             })
 
             .then(jsonData =>{
-                console.log(jsonData.data.children)
+                // console.log(jsonData.data.children)
+                // const dataArr = jsonData.data.children
+                // function containsImg(data){
+                //     if(data.thumbnail == true){
+                //         return true
+                //     }
+                //     else{return false} 
+                // }
+                // const imgArr = dataArr.filter(containsImg())
+                // console.log(imgArr)
                 jsonData.data.children.forEach(result => {
                     const img = document.createElement('img')
-                    img.src = result.data.thumbnail
-                    div.appendChild(img)
+                    slideDiv.appendChild(img)
+                    setInterval(() => {
+                        img.src = result.data.thumbnail
+                    }, 2000); 
+
+                    
                 });
                 const clearButton = document.createElement('button')
                 clearButton.style.display = "inline"
                 clearButton.innerText = "clear"
                 body.append(clearButton)
                 clearButton.addEventListener('click', (e) => {
-                    while(div.firstChild){
-                        div.innerHTML = ''
+                    while(slideDiv.firstChild){
+                        slideDiv.innerHTML = ''
                         break
                     }
                     section.style.display = "flex"
