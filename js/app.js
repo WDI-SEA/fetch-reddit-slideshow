df052b4287a32db10dfe78aa73a5a0c9c6df7644
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-29 18:35:00 -0700

    adding slide function

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ const input = document.querySelector('#searchBar')
 const button = document.querySelector('button')
 const photoZone = document.querySelector('#photoContainer')
 
+let photoSlide = false
+
+let photoArray = []
+
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-    // console.log('working')
     let search = input.value
     input.value = ''
     input.style.display = 'none'
     button.innerText = 'END'
-    button.addEventListener('click', () => {
-        console.log('working')
-        input.style.display = 'flex'
-    })
+    photoSlide = true
     fetch(`http://www.reddit.com/search.json?q=${search}`)
         .then(searchData => {
             return searchData.json()
@@ -21,27 +21,20 @@ form.addEventListener('submit', (e) => {
         .then(searchDataJson => {
             let children = searchDataJson.data.children
             children.forEach(photo => {
-                setInterval(() => {
-                    console.log(photo.data.url)
-                    // let photoUrl = children.data.url
-                    // const img = document.createElement('img')
-                    // img.src = photoUrl
-                    // photoZone.append(img)
-                }, 3000)
-                
-            });
-            
-            // for (let i = 0; i < 20; i++) {
-            //     if (children.is_video === false ) {
-            //         // console.log(children)
-            //         let photoUrl = children.url
-            //         const img = document.createElement('img')
-            //         img.src = photoUrl
-            //         photoZone.append(img)
-            //         count++
-            //     }
-            //     console.log(count)
-
-            // }
+                if (photo.data.is_video === false && photo.data.over_18 === false) {
+                    photoArray.push(photo.data.url)
+                }
             })
         })
+    })
+
+    function slidePhotos() {
+        if (photoSlide) {
+            for (let i = 0; i < photoArray.length; i++) {
+                setInterval(() => {
+                    
+                }, 3000)
+
+            }
+        }
+    }
