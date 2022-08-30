8d5eeee88692dd0a89d044c633e754373d24f1b4
Moodybleu <meganbenn27@icloud.com>
2022-08-30 07:30:31 -0700

    started sudocode

---
+++ js/script.js
@@ -0,0 +1,4 @@
+// step 1 -- fetch the reddit images by URL
+// step 2 -- Jsonify the data
+// step 3 -- Display the images in a carousel slideshow
+// step 4 -- be a good programmer and handle errors
