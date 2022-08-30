1d710d6c6e8c7836cfa85815af35da4e6b893460
joshdewhurst <joshuardewhurst@gmail.com>
2022-08-30 10:03:59 -0700

    commit 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 console.log('i am working')
 
-// document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener('DOMContentLoaded', () => {
+    let imageContainer = document.querySelector('#image-container')
     const button = document.querySelector('#button')
+    let stopBtn = document.querySelector('#stop-btn')
 
-    button.addEventListener("click", () => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
         const imgInput = document.querySelector('#search').value
         const url = `http://www.reddit.com/search.json?q=${imgInput}+nsfw:no` 
+        let imageContainer = document.querySelector('#image-container')
 
         fetch(url)
         .then(inputData => {
@@ -16,13 +19,27 @@ console.log('i am working')
             const datas= jsonData.data.children
             datas.forEach(data => {
                 const div = document.querySelector('div')
-                console.log(data.data)
+                // console.log(data.data)
                 const img = document.createElement('img')
                 img.src = data.data.thumbnail
                 div.append(img)
             })
+            // let i = 0
+            // jsonArray[i].classList.remove('active')
+
+            // let imageDisplayInterval = setInterval(() => {
+            //     jsonArray[i].classList.remove('active')
+
+            //     i++
+
+            //     if( i === jsonArray.length) {
+            //         i = 0
+            //     }
+
+            //     jsonArray[i].classList.add('active')
+            // }, (5000))
 
         })
     })
 
-// } )
+} )
