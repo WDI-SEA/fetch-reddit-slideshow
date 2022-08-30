ce333d8452fdb1f9a266716af3d87ba9700d631f
elek8871 <elekmichelle@gmail.com>
2022-08-29 21:30:35 -0600

    adds urls

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
-const submitBtn = document.querySelector("#search")
 
+const submitBtn = document.querySelector("#search")
+const input = document.querySelector("input")
+const form = document.querySelector("form")
+const hideDiv = document.querySelector("#hide")
+const results = document.querySelector("#results")
+const body = document.querySelector("body")
 const options = {
     headers: {
         'Accept': 'application/json'
     }
 }
 
-submitBtn.addEventListener("click", () =>{
-    const input = document.querySelector("#searchInput")
+form.addEventListener('submit', e =>{
+    e.preventDefault()
     const searchValue = `http://www.reddit.com/search.json?q=${input.value}+nsfw:no`
-//step 1 fetch data
-    fetch (searchValue, options)
-//step 2 jsonify data
-    .then(response => response.json())
-//step 3 do something with json data
-    .then(getSearch => {
-        console.log(getSearch.data.children)
+    hideDiv.getElementsByClassName.visibility = "hidden"
+    console.log(input.value)
+    // step1 -fetch
+    fetch(searchValue, options)
+    //  step 2 - jsonify data
+     .then(response => {
+        return response.json()
+     })
+    // step3 do something w json data
+    .then(search => {
+        // console.log(search.data.children)
+        const img = search.data.children
+        console.log(img[0].data.thumbnail)
+    
     })
-
+    // step 4 handle erroes
     .catch(err => {
         console.warn(err)
-    })
 })
-// grab search fxn coming into imput
+})
+})
+
 // create img gallery
 // display imngs
-// 
 
-})
 
 
 
