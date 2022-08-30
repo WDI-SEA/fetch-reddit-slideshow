a68badfb2ab24930029a698096fd352caf1b9b50
Moodybleu <meganbenn27@icloud.com>
2022-08-30 07:55:16 -0700

    Trying to get comments to show

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
 // step 1 -- fetch the reddit images by URL
+fetch('http://www.reddit.com/search.json?q=sunflowers+nsfw:no')
 // step 2 -- Jsonify the data
+.then(sunflowerImages => {
+    // console.log(sunflowerImages)
+    return sunflowerImages.json()
+})
+.then(returnedSunflowers => {
+    // console.log(returnedSunflowers.results)
+    //grab the document body
+    const body = document.querySelector('body')
+
+    returnedSunflowers.results.forEach(result => {
+        const div = document.createElement('div')
+        const h2 = document.createElement('h2')
+        h2.innerText = results.title
+        const p = document.createElement('p')
+        console.log(result.title)
+    })
 // step 3 -- Display the images in a carousel slideshow
 // step 4 -- be a good programmer and handle errors
