0833896b7e9528c850f88331b1ddafb385bf5a5b
brandon-w1205 <bwu1205@gmail.com>
2022-08-29 17:52:04 -0700

    adds url fixture for under 18 friendly content

--- js/script.js
+++ js/script.js
@@ -4,23 +4,30 @@ addEventListener('DOMContentLoaded', () => {
     const startButton = document.querySelector('#startSlide')
     const stopButton = document.querySelector('#stopSlide')
     let buttonValue = ""
+    let pictureArray = [];
 
     // Search button event listener for click
     startButton.addEventListener('click', (e) => {
         e.preventDefault()
         // sets the the variable "buttonValue" to whatever is in the Search box
         buttonValue = typingArea.value
-        const url = `https://www.reddit.com/search.json?q=${buttonValue}+nsfw:no`
+
+        // sets url to search up anything on reddit that is under 18 friendly content
+        const url = `https://www.reddit.com/search.json?q=${buttonValue}&over_18=false`
 
         fetch(url)
             .then(theData => {
                 return theData.json()
             })
             .then(dataJson => {
-                console.log(dataJson.data.children[1])
+                for(let i = 0; i < dataJson.data.children.length; i++) {
+                    console.log(dataJson.data.children[i].data.url)
+                    pictureArray.push(dataJson.data.children[i].data.url)
+                }
+                
+                
             })
     })
 
-    // creates url that allows you to search whatever on reddit without nsfw content
     
 })
