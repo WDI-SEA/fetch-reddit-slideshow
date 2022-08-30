f7017c997387d412485b7a522b1521eb9380c78e
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-08-30 09:08:27 -0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 })
 
 const button = document.getElementById("search");
@@ -12,60 +12,60 @@ function searchReddit() {
 
 
     fetch(`http://www.reddit.com/search.json?q=${search}+nsfw:no`, {
-        
-      })
-      .then(redditData => {
-        return redditData.json()
+
     })
-    .then(jsonData => {
-        //hides search section
-        document.querySelectorAll(".hide").forEach(function(el){
-            el.classList.add('hidden')
+        .then(redditData => {
+            return redditData.json()
         })
-        //hides slideshow on stop
-        document.querySelector(".slideshow").classList.remove('hidden')
-        
-        
-        let outputArray = []
-        const output = jsonData.data.children.map(child=> child.data.thumbnail);
-       for(i = 0; i < output.length; i++){
-        outputArray.push(output[i])
-       }
-       console.log(outputArray)
+        .then(jsonData => {
+            //hides search section
+            document.querySelectorAll(".hide").forEach(function (el) {
+                el.classList.add('hidden')
+            })
+            //hides slideshow on stop
+            document.querySelector(".slideshow").classList.remove('hidden')
 
-       // creates stop button
-       
-       var button = document.createElement('button');
-       button.innerText = "Stop";
-       button.addEventListener ("click", function() {
-        timer = 0
-        document.querySelectorAll(".hide").forEach(function(el){
-            el.classList.remove('hidden')
+
+            let outputArray = []
+            const output = jsonData.data.children.map(child => child.data.thumbnail);
+            for (i = 0; i < output.length; i++) {
+                outputArray.push(output[i])
+            }
+            console.log(outputArray)
+
+            // creates stop button
+            //remove hidden class. remove stop button. reset output array
+            var button = document.createElement('button');
+            button.innerText = "Stop";
+            button.addEventListener("click", function () {
+                timer = 0
+                document.querySelectorAll(".hide").forEach(function (el) {
+                    el.classList.remove('hidden')
+                })
+                document.querySelector(".slideshow").classList.add('hidden')
+                document.querySelector(".slideshow").removeChild(button);
+                let outputArray = []
+                outputArray.length = 0;
+            });
+            document.querySelector(".reddit").style.height = "300px";
+            document.querySelector(".reddit").style.width = "300px";
+            document.querySelector(".slideshow").appendChild(button);
+
+
+            var img = document.querySelector(".reddit")
+
+            var i = 0
+            var timer = setInterval(function () {
+                if (i > outputArray.length) {
+                    clearInterval(timer)
+                    return;
+                }
+                img.src = outputArray[i++];
+            }, 5000)
+
         })
-        document.querySelector(".slideshow").classList.add('hidden')
-        document.querySelector(".slideshow").removeChild(button);
-        let outputArray = []
-        outputArray.length = 0;
-      });
-       document.querySelector(".reddit").style.height = "300px";
-       document.querySelector(".reddit").style.width = "300px";
-       document.querySelector(".slideshow").appendChild(button);
+    console.log(outputArray)
+        .catch(err => console.warn(err))
 
 
-      var img = document.querySelector(".reddit")
-
-      var i = 0
-      var timer = setInterval(function(){
-        if (i > outputArray.length){
-            clearInterval(timer)
-            return;
-        }
-        img.src = outputArray[i++];
-      }, 5000)
-  
-})
-
-    .catch(err => console.warn(err))
-
-
-    }
+}
