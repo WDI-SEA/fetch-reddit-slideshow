0eb5d5bdf1e43cf864a3ff41b41f632329822207
Manion-sebastian <Sjmanion123@gmail.com>
2022-08-29 20:14:15 -0700

    hiding functionality working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const form = document.querySelector('form')
 const input = document.querySelector('#searchBar')
-const button = document.querySelector('button')
+const sButton = document.querySelector('#searchButton')
+const rButton = document.querySelector('#return')
 const photoZone = document.querySelector('#photoContainer')
 const title = document.querySelector('#title')
+
 
 
 let photoArray = []
@@ -10,10 +12,7 @@ let photoArray = []
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     let search = input.value
-    input.value = ''
-    input.style.display = 'none'
-    button.innerText = 'END'
-    title.style.display = 'none'
+    hideStart()
     fetch(`http://www.reddit.com/search.json?q=${search}`)
         .then(searchData => {
             return searchData.json()
@@ -21,22 +20,42 @@ form.addEventListener('submit', (e) => {
         .then(searchDataJson => {
             let children = searchDataJson.data.children
             children.forEach(photo => {
-                if (photo.data.is_video === false && photo.data.over_18 === false) {
+                if (photo.data.is_video === false && photo.data.domain === "i.redd.it") {
                     photoArray.push(photo.data.url)
                 }
             })
+            let photoNum = 0
+            const photo = document.createElement('img')
+            photo.src = photoArray[photoNum]
+            photoZone.append(photo)
+            const photoScroll = setInterval(() => {
+                while (photoZone.firstChild) {
+                    photoZone.removeChild(photoZone.firstChild);
+                }
+                if (photoNum === photoArray.length -1) {
+                    clearInterval(photoScroll)
+                    showEnd()
+                }
+                photoNum++
+                photo.src = photoArray[photoNum]
+                photoZone.append(photo)
+                
+            }, 5000);
         })
-        slidePhotos()
+
     })
 
-    function slidePhotos() {
-            photoArray.forEach(photo => {
-                let i = 0
-                setInterval(() => {
-                    const image = document.createElement('img')
-                    image.src = photo[i]
-                    photoZone.append(image)
-                    i++
-                }, 3000);
-            })
-        }
+function hideStart() {
+    input.value = ''
+    input.style.display = 'none'
+    title.style.display = 'none'
+    sButton.style.display = 'none'
+    rButton.style.displey = 'block'
+}
+
+function showEnd() {
+    input.style.display = 'flex'
+    title.style.display = 'flex'
+    sButton.style.display = 'flex'
+    rButton.style.displey = 'none'
+}
