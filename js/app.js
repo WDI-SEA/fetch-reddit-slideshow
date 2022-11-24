2a37b7f51be842b3b1dbb688cf24332bdd9dfd05
jaywomack <jaredwomack@Jareds-MBP.lan>
2022-11-23 19:05:13 -0800

    Fetching query and adding to the dom. Reset button functional. Not styling, per instruction

--- js/app.js
+++ js/app.js
@@ -1,59 +1,69 @@
-// const main = document.querySelector(".main")
-// fetch("https://dummyjson.com/products")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log(data.products)
-//     data.products.forEach((product) => {
-
-//     })
-//   })
-
-// //<div class="card" style="width: 18rem;">
-// //<img src="..." class="card-img-top" alt="...">
-// //<div class="card-body">
-// //<h5 class="card-title">Card title</h5>
-// //<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-// //<a href="#" class="btn btn-primary">Go somewhere</a>
-// //</div>
-// //</div>
+// DOM SELECTORS
+const form = document.querySelector(".input-form")
 const main = document.querySelector(".main")
-fetch("http://www.reddit.com/search.json?q=cat+nsfw:no")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.data.children[0].data)
-    //<div class="card" style="width: 18rem;">
-    const card = document.createElement("div")
-    card.classList.add("card", "mb-5")
-    card.style.width = "18rem"
-    //<img src="..." class="card-img-top" alt="...">
-    const cardImgTop = document.createElement("img")
-    cardImgTop.src = data.data.children[3].data.url
-    cardImgTop.classList.add("card-img-top")
-    cardImgTop.alt = "card alt tag"
-    //<div class="card-body">
-    const cardBody = document.createElement("div")
-    cardBody.classList.add("card-body")
-    //<h5 class="card-title">Card title</h5>
-    const cardTitle = document.createElement("h5")
-    cardTitle.classList.add("card-title")
-    cardTitle.textContent = data.data.children[3].data.title
-    //     cardTitle.textContent = product.title
-    //<p class="card-text"></p>
-    const cardText = document.createElement("p")
-    cardText.classList.add("card-text")
-    //     cardText.textContent = product.description
-    //<a href="#" class="btn btn-primary">Go somewhere</a>
-    //     const anchor = document.createElement("a")
-    //     anchor.href = "#"
-    //     anchor.classList.add("btn", "btn-primary")
-    //     anchor.textContent = "Add to Cart"
-
-    //</div>
-    //</div>
-    card.appendChild(cardImgTop)
-    card.appendChild(cardBody)
-    cardBody.appendChild(cardTitle)
-    cardBody.appendChild(cardText)
-    //     cardBody.appendChild(anchor)
-    main.appendChild(card)
+const textBtn = document.getElementById("input-btn")
+const textInput = document.getElementById("text-input")
+const imgContainer = document.querySelector(".image-container")
+let query = ""
+// reset button
+const resetButton = document.createElement("button")
+resetButton.classList.add("btn", "btn-danger", "w-100")
+resetButton.textContent = "Reset"
+// RESET CLICK LISTENER
+resetButton.addEventListener("click", (e) => {
+  e.preventDefault()
+  location.reload()
+})
+// INPUT SUBMIT CLICK EVENT
+textBtn.addEventListener("click", (e) => {
+  e.preventDefault()
+  query = textInput.value
+  fetch(`http://www.reddit.com/search.json?q=${query}+nsfw:no`, {
+    Accept: "application/json",
   })
+    .then((res) => res.json())
+    .then((data) => {
+      while (main.firstChild) {
+        main.removeChild(main.firstChild)
+      }
+      main.appendChild(resetButton)
+
+      const posts = data.data.children
+
+      posts.forEach((post) => {
+        const card = document.createElement("div")
+        card.classList.add("card", "mt-5", "text-center")
+        card.style.width = "18rem"
+
+        const cardImgTop = document.createElement("img")
+        cardImgTop.src = post.data.url
+        cardImgTop.classList.add("card-img-top")
+        cardImgTop.alt = "card alt tag"
+
+        const cardBody = document.createElement("div")
+        cardBody.classList.add("card-body")
+
+        const cardTitle = document.createElement("h5")
+        cardTitle.classList.add("card-title")
+        cardTitle.textContent = post.data.title
+
+        const cardText = document.createElement("p")
+        cardText.classList.add("card-text")
+        cardText.textContent = `Author: ${post.data.author}`
+
+        const anchor = document.createElement("a")
+        anchor.href = `https://www.reddit.com/${post.data.permalink}`
+        anchor.target = "_blank"
+        anchor.classList.add("btn", "btn-success")
+        anchor.textContent = "See on Reddit.com"
+        anchor.id = "anchor"
+
+        card.appendChild(cardImgTop)
+        card.appendChild(cardBody)
+        cardBody.appendChild(cardTitle)
+        cardBody.appendChild(cardText)
+        cardBody.appendChild(anchor)
+        main.appendChild(card)
+      })
+    })
+})
