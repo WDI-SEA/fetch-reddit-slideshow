a99a118d56bb8f55533a93d1524d4ddec7eb0f73
Turtle-Sprite <crystal.sheeley13@gmail.comcd>
2022-11-27 22:51:24 -0800

    added a bit of styling

--- script.js
+++ script.js
@@ -49,15 +49,12 @@ document.addEventListener('DOMContentLoaded', () => {
     
 //event listener for search button pressed, main wrapper
     search.addEventListener('click', function (e) {
+        e.preventDefault()
         //hide openPage class items
         hideStart()
 
-        //Could add a load page here and move unhideSlides into the next function...think about it
-
         //unhide slideshow
         unhideSlides ()
-        
-        e.preventDefault()
         
         //update the URL first because it didn't have any value outside of teh event listener - check
         requestURL = `http://www.reddit.com/search.json?q=${input.value}+nsfw:no`
@@ -121,9 +118,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 newImg.src = url
                 console.log(newImg.src)
                 //add parameters for making the images a certain size
-                newImg.style.height = "200px"
-                newImg.style.width = "200px"
-                newImg.style.border = "3px solid blue"
+                newImg.style.height = "400px"
+                newImg.style.width = "400px"
+                newImg.style.border = "3px solid purple"
     
                 //append it to the imagesDiv
                 imagesDiv.append(newImg)
@@ -138,13 +135,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
         }
         
-        //this starts with image array 1, cycles through all the array and loops around again
+        //this starts with image array 0, cycles through all the array and loops around again
         let index = 0
         addNewImg (newArray[index])
         let newSlideshow = setInterval(function() {
             addNewImg(newArray[index])
         }, 5000)
 
+        //reset button functionality
         stop.addEventListener('click', function () {
             clearInterval(newSlideshow)
             hideSlides()
@@ -155,70 +153,13 @@ document.addEventListener('DOMContentLoaded', () => {
                 while(imagesDiv.firstChild) { 
                     imagesDiv.removeChild(imagesDiv.firstChild)
                 }
-        })
-        // newArray.forEach(function(url){
-        // addNewImg(url)
-        // })
-        //this will update the image every 10 seconds using set interval
-        //it calls each image in array, sets an interval for displaying new image, then runs to the next image in display
+            }) 
 
-        //this loops through the array's length
-        
-            //this specifies a link in the array
+        })//then reddit data
+
+    })//fetch request
             
-            // console.log(newArray[i])
-            //take the link, put it into a function that changes the image
-            // addNewImg(changeLink)
-            //above updates the image, but they cycle through too quickly. Need to stop it from going so fast by adding a set interval...where?
+})//content loaded
 
-            //we are only seeing teh last image...It is caclling the function within setInterval
-            //it is running through ALL of the options and getting stuck on the last one
-            //it is not waiting 25 seconds to call the new array index
-            
-        
-        // setInterval(function() {
-        //     for(let i = 0; newArray.length > i; i++){
-        //     let changeLink = newArray[i]
-        //     addNewImg(changeLink)
-        //     console.log(changeLink, " ", i)
-        //     }
-        // }, 10000)
-        // newArray.forEach(addNewImg(url) {
-        //     setInterval(addNewImg, 10000)
-        // })
-
-        //setting a timer
-
-            //Great! I can append new images and erase old ones. Now that needs to happen every 10 seconds. 
-
-            //these images need to cycle through at an interval of 10 seconds
-            //these images should repeat after the newArray is finished, and return to 0
-            //these images should stop, if we press the stop button (call the function to hide slide and start the page again)
-
-             //this should set an interval for the length of time an image is displayd
-             
-
-            //every ten seconds get a new index in the array below
-            //add only the images to a global array (filter function)
-
-            //use timeout functions (set & clear Intervals) to let images display for a short time
-
-            
-            })//then reddit data
-
-        })//fetch request
-            
-    })//content loaded
-
-
-//button to stop slideshow
     
-    
-
-
-// * Create an array of image URLs (tip: use [filter](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) and [map](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map)).
-// * Cycle through images
-//     * tip: use [setInterval](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/setInterval)
-//     * Either add images, or change the `src` of a single image tag
 // * Add some interesting style / animation
-// * Create button to stop animation (tip: use [clearInterval](https://developer.mozilla.org/en-US/docs/Web/API/WindowTimers/clearInterval)).
