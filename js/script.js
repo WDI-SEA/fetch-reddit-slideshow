a588d4462db76e1494894575feb508bf25f1bd98
Andrew <andrew@andrewsmith.org>
2023-03-14 21:42:26 -0400

    Fetch results for static search query on form submit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log("hi 👋");
+let searchInput = document.querySelector("#searchInput");
+let value = searchInput.value;
+let redditSearchURL = "http://www.reddit.com/search.json?q=cats+nsfw:no";
+let fetchOptions = {};
+
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(value);
+  fetch(redditSearchURL, fetchOptions)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+    })
+    .catch(console.warn);
+});
