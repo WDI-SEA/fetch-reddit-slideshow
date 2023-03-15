2772e756151d696011678a150277adbf18c84be8
Andrew <andrew@andrewsmith.org>
2023-03-15 00:18:26 -0400

    Clean up superfluous console.logs

--- js/script.js
+++ js/script.js
@@ -8,23 +8,18 @@ let fetchedURLs = [];
 
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(value);
   fetch(redditSearchURL, fetchOptions)
     .then((response) => response.json())
     .then((jsonData) => {
       fetchedURLs = [];
-      console.log(jsonData.data.children);
       let imageBatchObjects = jsonData.data.children;
-      console.log(imageBatchObjects[0].data.url);
       imageBatchObjects.forEach((object) => {
         if (object.data.url) {
           fetchedURLs.push(object.data.url);
         }
       });
-      console.log(fetchedURLs);
       let slide = document.createElement("img");
       slide.src = fetchedURLs[0];
-      console.log(slide);
       slideshow.append(slide);
       setInterval(switchImage, 1000);
     })
@@ -35,6 +30,5 @@ const switchImage = () => {
   if (fetchedURLs.length > 0) {
     fetchedURLs.shift();
     document.querySelector("img").src = fetchedURLs[0];
-    console.log(fetchedURLs[0]);
   }
 };
