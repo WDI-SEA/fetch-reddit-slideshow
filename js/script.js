2db2ab7b3535c7bcb0f3f5689f4f154babafa50b
Andrew <andrew@andrewsmith.org>
2023-03-14 22:11:55 -0400

    Clear fetched URL array on new fetch

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let searchInput = document.querySelector("#searchInput");
 let value = searchInput.value;
 let redditSearchURL = "http://www.reddit.com/search.json?q=cats+nsfw:no";
 let fetchOptions = {};
+let fetchedURLs = [];
 
 searchForm.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -10,7 +11,14 @@ searchForm.addEventListener("submit", function (e) {
   fetch(redditSearchURL, fetchOptions)
     .then((response) => response.json())
     .then((jsonData) => {
-      console.log(jsonData);
+      fetchedURLs = [];
+      console.log(jsonData.data.children);
+      let imageBatchObjects = jsonData.data.children;
+      console.log(imageBatchObjects[0].data.url);
+      imageBatchObjects.forEach((object) => {
+        fetchedURLs.push(object.data.url);
+      });
+      console.log(fetchedURLs);
     })
     .catch(console.warn);
 });
