cd70fd3949d3c1c53080b8c8a9387a532f9f3816
juanedcabrera <juanedcabrera@gmail.com>
2023-03-14 19:01:30 -0600

    Initial HTML

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// fetch with the input 
+
+// use filter and map to only get images (array)
+// make sure to just show the images
+
+// cycle through images with setInterval
+
+//.src and create new URL if possible
+
+
+
