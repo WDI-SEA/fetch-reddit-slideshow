c1b37011cf1a8f62560bb4a8f998c15c768f4671
juanedcabrera <juanedcabrera@gmail.com>
2023-03-14 20:04:41 -0600

    Found API image url

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// fetch with the input 
+// fetch with the input
 
 // use filter and map to only get images (array)
 // make sure to just show the images
@@ -7,36 +7,35 @@
 
 //.src and create new URL if possible
 
-
-
 // DOM Selectors
-const form = document.querySelector('form');
-const input = document.querySelector('input')
-const loadingMessage = document.querySelector('#loading-message')
-const slideShow = document.querySelector('#slideshow')
-const stopButton = document.querySelector('#stop-button')
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const loadingMessage = document.querySelector("#loading-message");
+const slideShow = document.querySelector("#slideshow");
+const stopButton = document.querySelector("#stop-button");
 
 // Reddit API URL
-const redditURL =  'http://www.reddit.com/search.json'
+const redditURL = "http://www.reddit.com/search.json";
 
 // Array to keep images
-let imageUrls = []
+let imageUrls = [];
 
 // submit form event listener - this should load pieces (loading message and clear input)
-form.addEventListener('submit', (e) =>) {
-    e.preventDefault();
-    const inputValue = input.value;
-    input.value = ''
-    fetch(`${redditURL}?q=${inputValue}+nsfw:no`)
-    .then(response => response.json())
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = input.value;
+  input.value = "";
+  fetch(`${redditURL}?q=${inputValue}+nsfw:no`)
+    .then((response) => response.json())
     .then(data => {
-        form.style.display = 'none'
-        loadingMessage.style.display = 'none'
+        console.log(data.data.children[0].data.url)
+    //   form.style.display = "none";
+    //   loadingMessage.style.display = "none"
         // something to add to my array - need to see API info
         // start slideshow
-        // catch error
     })
-}
+    .catch(console.warn)
+})
 
 // function for slideshow
 
