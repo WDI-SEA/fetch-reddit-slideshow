e9016b804847dddb0a6e5d3444719dcda43cdd3d
Heiner000 <garrett.heiner@gmail.com>
2023-03-15 22:17:42 -0700

    reddit deliverable functionally finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,65 @@
-const searchWord = document.querySelector("#searchWord")
-const keyword = searchWord.value
 const startButton = document.querySelector("#startButton")
-const redditUrl = `https://reddit.com/search/json?q=${keyword}+nsfw:no`
-const images = []
+const stopButton = document.querySelector("#stopButton")
+const searchForm = document.querySelector("#searchForm")
+const userInput = document.querySelector("#userInput")
+
+let slides = []
 let count = 0
-let runSlideInterval = false
+let runInterval
+console.log(runInterval)
 
-   
-startButton.addEventListener("click", () => {
-    console.log(keyword)
+console.log(startButton, stopButton, searchForm, userInput)
 
-    startButton.addEventListener("click", () => {
-        console.dir(keyword)
-
-        fetch(redditUrl)
-            .then(results => results.json())
-            .then(console.log)
-            .catch(console.warn)
-    })
-
+// submit the form and fetch the reddit data
+searchForm.addEventListener("submit", e => {
+    e.preventDefault()
+    let keyword = userInput.value
+    let redditUrl = `http://www.reddit.com/search.json?q=${keyword}+nsfw:no`
+    console.log(keyword, redditUrl)
+    fetch(redditUrl)
+        .then(rawData => rawData.json())
+        .then((jsonData) => {
+            console.log(jsonData)
+            let filteredData = jsonData.data.children.filter(function(child) {
+                return child.data.url.endsWith(".jpg") || child.data.url.endsWith(".png")
+            })
+            slides = filteredData.map(function(child) {
+                return child.data.url
+            })
+            console.log(slides)
+            runSlideshow()
+        })
+        .catch(console.warn)
 })
 
+const imageDiv = document.querySelector(".imageDiv")
+const titleCard = document.querySelector(".titleCard")
 
+// get the slideshow going w/ intervals
+function runSlideshow() {
+    count = 0
+    let img = document.createElement("img")
+    img.src = slides[count]
+    imageDiv.appendChild(img)
+    runInterval = setInterval(function() {
+        count++
+        if (count >= slides.length) {
+            count = 0
+        }
+        img.src = slides[count]
+    }, 2000)
+    titleCard.style.visibility = "hidden"
+    imageDiv.style.visibility = "visible"
+    stopButton.style.visibility = "visible"
+}
 
-
-// When the user enters a search term and presses enter, the form / title / description should hide
-    // Show a loading message (optional)
-    // Fetch related posts from reddit (with fetch)
-    // Display animation / slideshow of images (with DOM manipulation)
-    // Show a button to stop slideshow
-    // Repeat animation until user clicks "stop", then redisplay the original form/title/description
-// When the user clicks the "stop" button
-    // Animation stops / images are removed
-    // Form / title / description are shown again
-    // User can enter a new search term
-
-// Suggested Process
-    // Create your form (HTML/CSS)
-    // Prevent default form submission and verify that you can type something into the form
-    // Use fetch to make a request. Show data in console
-    // Create an array of image URLs (tip: use filter and map).
-    // Make the form / title / description hide
-    // Cycle through images
-        // tip: use setInterval
-        // Either add images, or change the src of a single image tag
-    // Add some interesting style / animation
-    // Create button to stop animation (tip: use clearInterval).
+// stop the slideshow and go back to how things were without refreshing the page
+stopButton.addEventListener("click", function() {
+    clearInterval(runInterval)
+    imageDiv.innerHTML = ""
+    slides = []
+    userInput.value = ""
+    imageDiv.style.visibility = "hidden"
+    titleCard.style.visibility = "visible"
+    stopButton.style.visibility = "hidden"
+})
