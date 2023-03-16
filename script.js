22759a25616280cafc2a5f10979e691bc076df54
jaronw1 <jaronwong6@gmail.com>
2023-03-16 10:11:34 -0700

    data slideshow Unfinished

--- script.js
+++ script.js
@@ -1,30 +1,72 @@
 
 //select form input
 let formInput = document.querySelector("#formInput")
+let slideshowElem = document.querySelector("#slideshow")
+
+//acess images
+
+let resultImages
+
+
 function fetchReddit(e){
-    //e.preventDefault()
+    e.preventDefault()
     //let value = "cats"
     fetch(`http://www.reddit.com/search.json?q=${formInput.value}cats+nsfw:no`)
     .then(result => result.json())
     .then(results => {
-        console.log(results.data.children)
+        //console.log(results.data.children)
         let resultImages = results.data.children.map[child => {
             return{
                 url: child.data.url,
                 title: child.data.title
             }
         }]
+
+        .filter(image => {
+            let imgExtension = img.url.slice(-4)
+            return imgExtension === ".jpg" || imgExtension === ".png"
+
+        })
         console.log(resultImages)
+
+        slideshow(resultImages)
+        let slideshowInterval = setInterval(slideshow, 1000)
+
+
     })
     
     .catch(console.warn)
+
 }
 //fetchReddit()
 
-let stopBtn = document.querySelector("stopBtn")
+//slideshow function
+let imgIndex = 0
+function slideshow(imgArr) {
+    console.log("imgs =>", imgArr)
+    console.log("el =>", slideshowElem.src)
+    slideshowElem.src = imgArr[0].url
+    if (imgIndex >= imgArr.length) {
+        imgIndex = 0
+    }
+    slideshowElem.src = imgArr(imgIndex).url
+    imgIndex = imgIndex + 1
+}
+
+
+
+
+
+
+
+
+let stopBtn = document.querySelector("#stopBtn")
 stopBtn.addEventListener("click", function(){
-    console.log("stop")
+    console.log("stopit")
 })
+
+
+
 
 let submitBtn = document.querySelector("#submitButton")
 submitBtn.addEventListener("click", fetchReddit)
