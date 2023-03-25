7c0d7cee998d747a97ca67b075364e80e6df4b70
dxk0ta <dxkotacamps@gmail.com>
2023-03-24 20:48:52 -0700

    finished

--- scripts.js
+++ scripts.js
@@ -1,43 +1,42 @@
+
+let formInput = document.querySelector("#formInput")
+let slideshowEl = document.querySelector("#slideshow")
+let resultImages
+let slideshowInterval
+
 function fetchReddit(e) {
     e.preventDefault()
-    let formInput = document.querySelector("#formInput")
-    let slideshowEl = document.querySelector("slideshow")
-    console.log("target =>", formInput.value)
+   
+    console.log("input =>",formInput.value)
     fetch(`http://www.reddit.com/search.json?q=${formInput.value}+nsfw:no`)
         .then(result => result.json())
         .then(results => {
-            let resultImages = results.data.children.map(child => {
-            //console.log(results.data.children.map) (child => {
+            
+            resultImages = results.data.children.map(child => {
                 return {
                     url: child.data.url,
                     title: child.data.title
                 }
             })
-
+           
             .filter(image => {
-                let imageExtention = image.url.slice(-4)
-                return imageExtention === ".jpg" || imageExtention === ".png"
+                let imgExtension = image.url.slice(-4)
+                return imgExtension === ".jpg" || imgExtension === ".png"
             })
-        slideshow(resultImages)
-        
-        let slideshowInterval = setInterval
-        (slideshow, 3000)
-        // easy to invoke a function and pass images as an argument //
+            console.log("inside fetch", resultImages)
+           
+            let slideshowInterval = setInterval(slideshow, 1000)
+            slideshowInterval = setInterval(() => {
+                slideshow(resultImages)
+            }, 3000)
+
         })
         .catch(console.warn)
-}
-
-function slideshow(imgArr) { 
-    console.log("imgs =>", imgArr)
-    console.log("el =>", slideshoeEl.src)
-    if(imgIndex >= imgArr.length) {
-        imgIndex = 0
-    }
-    slideshowEl.src = imgArr[imgIndex].url
-    imgIndex = imgIndex
-}
-
-let stopBtn = document.querySelector("stopBtn")
+function slideshow(resultImages) {
+let stopBtn = document.querySelector("#stopBtn")
 stopBtn.addEventListener("click", function(){
     console.log("stop")
+    clearInterval(slideshowInterval)
 })
+let submitBtn = document.querySelector("#submitBtn")
+submitBtn.addEventListener("click", fetchReddit)
